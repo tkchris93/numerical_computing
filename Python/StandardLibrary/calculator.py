91fd3a55afc8800b2c0538d99a83446dc51e7c58
Shane <shanemcq18@gmail.com>
2015-06-25 18:45:49 -0600

    -m Final OOP changes. Also a forgotten lab1 file.

---
+++ Python/StandardLibrary/calculator.py
@@ -0,0 +1,17 @@
+# calculator.py
+""" Volume II Lab 1: The Standard Library (Auxiliary file)
+    Solutions file helper code. Main solution in 'lab1_solutions.py'.
+"""
+
+import math
+
+def add(x,y):
+    return x+y
+
+def mult(x,y):
+    return x*y
+
+def sqrt(x):
+    return math.sqrt(x)
+
+# Or "sqrt = math.sqrt"
