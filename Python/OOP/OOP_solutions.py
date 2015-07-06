d3709ee93e7ef2bd76dab8a110cb279ab5a8bc62
Shane <shanemcq18@gmail.com>
2015-07-06 14:09:19 -0600

    Lab 1, 2 improvements

--- Python/OOP/OOP_solutions.py
+++ Python/OOP/OOP_solutions.py
@@ -1,8 +1,6 @@
 # solutions.py
 """Volume II Lab 2: Object Oriented Programming
-    Main solutions file. See also 'Backpack_solutions.py' and 'Knapsack.py'.
-    Use the test() method as a test script.
-    Written by Shane McQuarrie, Spring 2015.
+    Solutions file. Written by Shane McQuarrie, Spring 2015.
 """
 
 # Students should have their 'Backpack' class in a 'Backpack.py' file and
@@ -261,9 +259,9 @@ class ComplexNumber(object):
         # return ComplexNumber(numer.real/denom, numer.imag/denom)
 
 
-# ----------------------------- END OF SOLUTIONS ----------------------------- #
+# =========================== END OF SOLUTIONS =========================== #
 
-# Test Script
+
 def test(student_module, late=False):
     """Test script. You must import the students file as a module.
     
@@ -283,37 +281,49 @@ def test(student_module, late=False):
     
     def strTest(p,m):
         """Manually grade a problem worth 'p' points with error message 'm'."""
-        part = p + 1
-        while part > p:
+        part = -1
+        while part > p or part < 0:
             part = int(input("\nScore out of " + str(p) + ": "))
         if part < p: return part,m
         else: return part,""
+    
+    def attrTest(x,y,m):
+        if x == y: return 1, ""
+        else:
+            message  = m
+            message += "\n\t\tCorrect response: " + str(x)
+            message += "\n\t\tStudent response: " + str(y)
+            return 0, message
     
     s = student_module
     score = 0
     feedback = s.__doc__
     print(feedback)
     
-    try:
-        # Problem 1: 10 points
-        feedback += "\nTesting problem 1 (10 points)..."
+    try:    # Problem 1: 10 points
+        feedback += "\n\nTesting problem 1 (10 points):"
         points = 0
         # Test default name and max_size attributes
         b1 =   Backpack()
         b2 = s.Backpack()
-        if b1.name == b2.name: points += 1
-        else: feedback += "\n\tBackpack.name failed on default value"
-        if b1.max_size == b2.max_size: points += 1
-        else: feedback += "\n\tBackpack.max_size failed on default value"
+        p,f = attrTest(b1.name, b2.name,
+            "\n\tBackpack.name failed on default value")
+        points += p; feedback += f
+        p,f = attrTest(b1.max_size, b2.max_size,
+            "\n\tBackpack.max_size failed on default value")
+        points += p; feedback += f
         # Test non-default color, name, and max_size attributes
-        b1 =   Backpack(color='asvnawie',name='efnaiwen', max_size=3)
-        b2 = s.Backpack(color='asvnawie',name='efnaiwen', max_size=3)
-        if b1.name == b2.name: points += 1
-        else: feedback += "\n\tBackpack.name failed on nondefault value"
-        if b1.max_size == b2.max_size: points += 1
-        else: feedback += "\n\tBackpack.max_size failed on nondefault value"
-        if b1.color == b2.color: points += 1
-        else: feedback += "\n\tBackpack.color failed on nondefault value"
+        b1 =   Backpack(color='teal',name='Francis', max_size=3)
+        b2 = s.Backpack(color='teal',name='Francis', max_size=3)
+        p,f = attrTest(b1.name, b2.name,
+            "\n\tBackpack.name failed on nondefault value")
+        points += p; feedback += f
+        p,f = attrTest(b1.max_size, b2.max_size,
+            "\n\tBackpack.max_size failed on nondefault value")
+        points += p; feedback += f
+        p,f = attrTest(b1.color, b2.color,
+            "\n\tBackpack.color failed on nondefault value")
+        points += p; feedback += f
         # Test put() (no going over the limit)
         b1.put('f'); b1.put('u'); b1.put('n')
         b2.put('f'); b2.put('u'); b2.put('n')
@@ -321,82 +331,91 @@ def test(student_module, late=False):
         print("Student output:\t"),; b2.put('this should not fit')
         p,f = strTest(1,"\n\tBackpack.put() failed to print 'Backpack Full!'")
         points += p; feedback += f
-        if b1.contents == b2.contents: points += 1
-        else: feedback += "\n\tBackpack.put() failed (went over max_size)"
+        p,f = attrTest(b1.contents, b2.contents,
+            "\n\tBackpack.put() failed (Backpack.contents over max_size)")
+        points += p; feedback += f
         # Test dump()
         b1.dump(); b2.dump()
-        if b1.contents == b2.contents: points += 1
-        else: feedback += "\n\tBackpack.dump() failed to empty contents"
+        p,f = attrTest(b1.contents, b2.contents,
+            "\n\tBackpack.dump() failed to empty contents")
+        points += p; feedback += f
         # Test docstrings
         print("\nStudent Backpack class docstrings:\n")
         print(b2.__doc__); print(b2.__init__.__doc__)
         p,f = strTest(2,"\n\tbad Backpack docstring(s)")
         points += p; feedback += f
         
-        score += points; feedback += "\n  Score += " + str(points)
+        score += points; feedback += "\nScore += " + str(points)
+    except Exception as e: feedback += "\nError: " + e.message
         
-        # Problem 2: 15 points
-        feedback += "\nTesting problem 2 (15 points)..."
+    try:    # Problem 2: 15 points
+        feedback += "\n\nTesting problem 2 (15 points):"
         points = 0
         # Test default name, and max_size attributes
         b1 =   Jetpack()
         b2 = s.Jetpack()
-        if b1.name == b2.name: points += 1
-        else: feedback += "\n\tJetpack.name failed on default value"
-        if b1.max_size == b2.max_size: points += 1
-        else: feedback += "\n\tJetpack.max_size failed on default value"
+        p,f = attrTest(b1.name, b2.name,
+            "\n\tJetpack.name failed on default value")
+        points += p; feedback += f
+        p,f = attrTest(b1.max_size, b2.max_size,
+            "\n\tJetpack.max_size failed on default value")
+        points += p; feedback += f
         # Test non-default color, name, and max_size attributes
-        b1 =   Jetpack(color='asvnawie',name='efnaiwen', max_size=3)
-        b2 = s.Jetpack(color='asvnawie',name='efnaiwen', max_size=3)
-        if b1.name == b2.name: points += 1
-        else: feedback += "\n\tJetpack.name failed on nondefault value"
-        if b1.max_size == b2.max_size: points += 1
-        else: feedback += "\n\tJetpack.max_size failed on nondefault value"
-        if b1.color == b2.color: points += 1
-        else: feedback += "\n\tJetpack.color failed on nondefault value"
+        b1 =   Jetpack(color='Gold',name='Golden Boy', max_size=3)
+        b2 = s.Jetpack(color='Gold',name='Golden Boy', max_size=3)
+        p,f = attrTest(b1.name, b2.name,
+            "\n\tJetpack.name failed on nondefault value")
+        points += p; feedback += f
+        p,f = attrTest(b1.max_size, b2.max_size,
+            "\n\tJetpack.max_size failed on nondefault value")
+        points += p; feedback += f
+        p,f = attrTest(b1.color, b2.color,
+            "\n\tJetpack.color failed on nondefault value")
+        points += p; feedback += f
         # Test fuel attribute
-        if b1.fuel == b2.fuel: points += 1
-        else: feedback += "\n\tJetpack.fuel failed on starting value"
+        p,f = attrTest(b1.fuel, b2.fuel,
+            "\n\tJetpack.fuel failed on default value")
+        points += p; feedback += f
         # Test put() (no going over the limit)
         b1.put('f'); b1.put('u'); b1.put('n')
         b2.put('f'); b2.put('u'); b2.put('n')
         print("\nCorrect output:\t"),; b1.put('this should not fit')
-        print(  "Student output:\t"),; b2.put('this should not fit')
-        p,f = strTest(1,"\n\tJetpack.put() failed to print 'Backpack Full!'")
+        print("Student output:\t"),; b2.put('this should not fit')
+        p,f = strTest(1,"\n\tBackpack.put() failed to print 'Backpack Full!'")
         points += p; feedback += f
-        if b1.contents == b2.contents: points += 1
-        else: feedback += "\n\tJetpack.put() failed (went over max_size)"
+        p,f = attrTest(b1.contents, b2.contents,
+            "\n\tJetpack.put() failed (Jetpack.contents over max_size)")
+        points += p; feedback += f
         # Test dump()
         b1.dump(); b2.dump()
-        if b1.contents == b2.contents: points += 1
-        else: feedback += "\n\tJetpack.dump() failed to empty contents"
-        if b1.fuel == b2.fuel: points += 1
-        else: feedback += "\n\tJetpack.dump() failed to empty fuel"
+        p,f = attrTest(b1.contents, b2.contents,
+            "\n\tJetpack.dump() failed to empty contents")
+        points += p; feedback += f
         # Test fly()
-        b1.fuel = 10
-        b2.fuel = 10
+        b1.fuel = 10; b2.fuel = 10
         print("\nCorrect output:\t"),; b1.fly(20)
         print(  "Student output:\t"),; b2.fly(20)
         p,f = strTest(1,"\n\tJetpack.put() failed to print 'Not enough fuel!'")
         points += p; feedback += f
-        if b1.fuel == b2.fuel: points += 1
-        else: feedback += "\n\tJetpack.fly() failed to decrease fuel correctly"
+        p,f = attrTest(b1.fuel, b2.fuel,
+            "\n\tJetpack.fly() failed to decrease fuel correctly")
+        points += p; feedback += f
         b1.fly(5); b2.fly(5)
-        if b1.fuel == b2.fuel: points += 1
-        else: feedback += "\n\tJetpack.fly() failed to decrease fuel correctly"
+        p,f = attrTest(b1.fuel, b2.fuel,
+            "\n\tJetpack.fly() failed to decrease fuel correctly")
+        points += (p*2); feedback += f        
         # Test docstrings
         print("\nStudent Jetpack class docstrings:\n")
         print(b2.__doc__); print(b2.__init__.__doc__)
         p,f = strTest(2,"\n\tbad Jetpack docstring(s)")
         points += p; feedback += f
-        if not issubclass(s.Jetpack, s.Backpack):
-            points = 0; feedback += "\n\tJetpack must inherit from Backpack!"
         
-        score += points; feedback += "\n  Score += " + str(points)
+        score += points; feedback += "\nScore += " + str(points)
+    except Exception as e: feedback += "\nError: " + e.message
         
-        # Problem 3: 15 points
+    try:    # Problem 3: 15 points
         points = 0
-        feedback += "\nTesting problem 3 (15 points)..."
+        feedback += "\n\nTesting problem 3 (15 points):"
         # Test __str__ on an empty, default backpack
         b1 =   Backpack()
         b2 = s.Backpack()
@@ -404,112 +423,128 @@ def test(student_module, late=False):
         print("\nStudent output:"); print(b2)
         p,f = strTest(3,"\n\tBackpack.__str__ failed")
         points += p; feedback += f
+        if p < 3: feedback += "\n\tStudent str(Backpack):\n\n" + str(b2) + '\n'
         # Test __str__ on a partially full backpack
-        b1 =   Backpack('Blue and White','ACME',7)
-        b2 = s.Backpack('Blue and White','ACME',7)
+        b1 =   Backpack(color='Blue and White',name='ACME',max_size=7)
+        b2 = s.Backpack(color='Blue and White',name='ACME',max_size=7)
         b1.put('thing1'); b1.put('thing2'); b1.put('thing3')
         b2.put('thing1'); b2.put('thing2'); b2.put('thing3')
         print("\nCorrect output:"); print(b1)
         print("\nStudent output:"); print(b2)
         p,f = strTest(5,"\n\tBackpack.__str__ failed")
         points += p; feedback += f
+        if p < 5: feedback += "\n\tStudent str(Backpack):\n\n" + str(b2) + '\n'
         # Test __eq__ in False case
-        b1 =   Backpack('Color','Name',5)
-        b2 = s.Backpack('Color','Name',5)
+        b1 = s.Backpack(color='Color',name='Name',max_size=5)
+        b2 = s.Backpack(color='Color',name='Name',max_size=5)
         b1.name = 'FalseName'
-        if b1 == b2: feedback += "Backpack.__eq__ failed on different names"
+        if b1 == b2:
+            feedback += "\n\tBackpack.__eq__ failed on different names"
         else: points += 1
         b1.name = 'Name'; b1.color = 'Red'
-        if b1 == b2: feedback += "Backpack.__eq__ failed on different colors"
+        if b1 == b2:
+            feedback += "\n\tBackpack.__eq__ failed on different colors"
         else: points += 1
         b1.color = 'Color'
-        b1.put('an item')
-        b2.put("a different item")
+        b1.put('an item'); b2.put("a different item")
         if b1 == b2:
-            feedback += "Backpack.__eq__ failed on different contents"
-            feedback += " (same sized contents lists)"
-        else: points += 1;
+            feedback += "\n\tBackpack.__eq__ failed on different contents"
+            feedback += "\n\t\tBackpack_1.contents: " + str(b1.contents)
+            feedback += "\n\t\tBackpack_2.contents: " + str(b2.contents)
+        else: points += 1
         b1.put("yet another item")
         if b1 == b2:
-            feedback += "Backpack.__eq__ failed on different contents"
-            feedback += " (different sized contents list)"
+            feedback += "\n\tBackpack.__eq__ failed on different contents"
+            feedback += "\n\t\tBackpack_1.contents: " + str(b1.contents)
+            feedback += "\n\t\tBackpack_2.contents: " + str(b2.contents)
         else: points += 1
         # Test __eq__ in True case
-        b1 =   Backpack('green','Math',3)
-        b2 = s.Backpack('green','Math',3)
-        if not (b1 == b2): feedback += "Backpack.__eq__ failed on equal"
+        b1 = s.Backpack(color='green',name='Math',max_size=3)
+        b2 = s.Backpack(color='green',name='Math',max_size=3)
+        if not (b1 == b2): feedback += "\n\tBackpack.__eq__ failed on equal"
         else: points += 1
         b1.put('an item'); b2.put('an item')
-        if not (b1 == b2): feedback += "Backpack.__eq__ failed on equal"
+        if not (b1 == b2): feedback += "\n\tBackpack.__eq__ failed on equal"
         else: points += 1
         b1.put('A'); b1.put('B')
         b2.put('B'); b2.put('A')
         if not (b1 == b2):
-            feedback += "Backpack.__eq__ failed on equal"
-            feedback += " (same contents in different order)"
+            feedback += "\n\tBackpack.__eq__ failed on equal"
+            feedback += "\n\t\tBackpack_1.contents: " + str(b1.contents)
+            feedback += "\n\t\tBackpack_2.contents: " + str(b2.contents)
+            feedback += "\n\t\t(two backpacks with the same contents"
+            feedback += "\n\t\t are equal even if the contents are in"
+            feedback += "\n\t\t a different order"
         else: points += 1
         
-        score += points; feedback += "\n  Score += " + str(points)
+        score += points; feedback += "\nScore += " + str(points)
+    except Exception as e: feedback += "\nError: " + e.message
         
-        # Problem 4: 20 points
-        feedback += "\nTesting problem 4 (15 points)..."
+    try:    # Problem 4: 20 points
+        feedback += "\n\nTesting problem 4 (15 points):"
         points = 0
         x1 =   ComplexNumber(3392,-493)
         y1 = s.ComplexNumber(3392,-493)
         # Test real / imag attributes
-        if x1.real == y1.real: points += 1
-        else: feedback += "\n\tComplexNumber.real failed"
-        if x1.imag == y1.imag: points += 1
-        else: feedback += "\n\tComplexNumber.imag failed"
+        p,f = attrTest(x1.real, y1.real, "\n\tComplexNumber.real failed")
+        points += p; feedback += f
+        p,f = attrTest(x1.imag, y1.imag, "\n\tComplexNumber.imag failed")
+        points += p; feedback += f
+        
+        
         # Test conjugate()
-        x2 = x1.conjugate()
-        y2 = y1.conjugate()
-        if x2.real == y2.real: points += 1
-        else: feedback += "\n\tComplexNumber.conjugate() failed for real part"
-        if x2.imag == y2.imag: points += 1
-        else: feedback += "\n\tComplexNumber.conjugate() failed for imag part"
+        x2 = x1.conjugate(); y2 = y1.conjugate()
+        p,f = attrTest(x2.real, y2.real,
+            "\n\tComplexNumber.conjugate() failed on real part")
+        points += p; feedback += f
+        p,f = attrTest(x2.imag, y2.imag,
+            "\n\tComplexNumber.conjugate() failed on imag part")
+        points += p; feedback += f
         # Test norm()
-        if x1.norm() == y1.norm(): points += 1
-        else: feedback += "\n\tComplexNumber.norm() failed"
+        p,f = attrTest(x1.norm(), y1.norm(),
+            "\n\tComplexNumber.norm() failed")
+        points += p; feedback += f
         # Test __add__
-        x2 = ComplexNumber(-21,210)
-        y2 = ComplexNumber(-21,210)
-        x3 = x1 + x2
-        y3 = y1 + y2
-        if x3.real == y3.real: points += 1
-        else: feedback += "\n\tComplexNumber.__add__ failed for real part"
-        if x3.imag == y3.imag: points += 1
-        else: feedback += "\n\tComplexNumber.__add__ failed for imag part"
+        x2 = ComplexNumber(-21,210); y2 = ComplexNumber(-21,210)
+        x3 = x1 + x2; y3 = y1 + y2
+        p,f = attrTest(x3.real, y3.real,
+            "\n\tComplexNumber.__add__ failed on real part")
+        points += p; feedback += f
+        p,f = attrTest(x3.imag, y3.imag,
+            "\n\tComplexNumber.__add__ failed on imag part")
+        points += p; feedback += f
         # Test __sub__
-        x3 = x1 - x2
-        y3 = y1 - y2
-        if x3.real == y3.real: points += 1
-        else: feedback += "\n\tComplexNumber.__sub__ failed for real part"
-        if x3.imag == y3.imag: points += 1
-        else: feedback += "\n\tComplexNumber.__sub__ failed for imag part"
+        x3 = x1 - x2; y3 = y1 - y2
+        p,f = attrTest(x3.real, y3.real,
+            "\n\tComplexNumber.__sub__ failed on real part")
+        points += p; feedback += f
+        p,f = attrTest(x3.imag, y3.imag,
+            "\n\tComplexNumber.__sub__ failed on imag part")
+        points += p; feedback += f
         # Test __mul__
-        x3 = x1 * x2
-        y3 = y1 * y2
-        if x3.real == y3.real: points += 1
-        else: feedback += "\n\tComplexNumber.__mul__ failed for real part"
-        if x3.imag == y3.imag: points += 1
-        else: feedback += "\n\tComplexNumber.__mul__ failed for imag part"
+        x3 = x1 * x2; y3 = y1 * y2
+        p,f = attrTest(x3.real, y3.real,
+            "\n\tComplexNumber.__mul__ failed on real part")
+        points += p; feedback += f
+        p,f = attrTest(x3.imag, y3.imag,
+            "\n\tComplexNumber.__mul__ failed on imag part")
+        points += p; feedback += f
         # Test __div__
-        x3 = x1 / x2
-        y3 = y1 / y2
-        if x3.real == y3.real: points += 1
-        else: feedback += "\n\tComplexNumber.__div__ failed for real part"
-        if x3.imag == y3.imag: points += 1
-        else: feedback += "\n\tComplexNumber.__div__ failed for imag part"
+        x3 = x1 / x2; y3 = y1 / y2
+        p,f = attrTest(x3.real, y3.real,
+            "\n\tComplexNumber.__div__ failed on real part")
+        points += p; feedback += f
+        p,f = attrTest(x3.imag, y3.imag,
+            "\n\tComplexNumber.__div__ failed on imag part")
+        points += p; feedback += f
         # Test docstrings
         print("\nStudent docstrings:\n")
         print(y1.__doc__); print(y1.__init__.__doc__)
         p,f = strTest(2,"\n\tbad ComplexNumber docstring(s)")
         points += p; feedback += f
         
-        score += points; feedback += "\n  Score += " + str(points)
-        
-    except Exception as e: feedback += "\n\nError: " + e.message
+        score += points; feedback += "\nScore += " + str(points)
+    except Exception as e: feedback += "\nError: " + e.message
     
     if late:    # Late submission penalty
         feedback += "\n\nHalf credit for late submission."
@@ -518,7 +553,6 @@ def test(student_module, late=False):
     
     # Report final score
     feedback += "\n\nTotal score: "+str(score)+"/55 = "+str(score/.55)+"%"
-    
     if   score/.55 >= 99.0: feedback += "\n\nExcellent!"
     elif score/.55 >=  90.0: feedback += "\n\nGreat job!"
     
