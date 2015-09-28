be729df77af2f0628402b3bc0f77fc1665caeea5
tkchris93 <tannerkchristensen@gmail.com>
2015-09-28 16:40:36 -0600

    created spec and solutions files. Still need to write test driver

--- Labs/AffineTransformations/old_solutions.py
+++ Labs/AffineTransformations/old_solutions.py
@@ -1,165 +1,110 @@
-'''
-    Invertible Affine Transformations
-    For this lab, you need to turn in this solutions.py file. Also place the 
-    horse.npy in your Google Drive folder for this section.
-'''
-
 import numpy as np
-import math
-from matplotlib import pyplot as plt
 from scipy import linalg as la
+from matplotlib import pyplot as plt
+import time
 
-def plotNewOld(new, old):
-    ''' 
-    This plotting script gives better results than the one provided in the lab
-    text. Please use this to plot your figures.
-    Inputs:
-    new -- a (2,n) numpy array containing the transformed x-coordinates on the 
-            first row, y-coordinates on the second row.
-    old -- a (2,n) numpy array containing the original x-coordinates on the first
-            row, y-coordinates on the second row.
+def plot_transform(original, new):
+    '''Inputs:
+    original -- a (2,n) numpy array containing x-coordinates on the first 
+                row and y-coordinates on the second row.
+    new -- a (2,n) numpy array containing x-coordinates on the first row 
+           and y-coordinates on the second row.
     '''
-    # Find the largest and smallest x- and y-values, used to set the size of the axes
-    x_max = np.max([np.max(old[0]), np.max(new[0])])
-    x_min = np.min([np.min(old[0]), np.min(new[0])])
-    y_max = np.max([np.max(old[1]), np.max(new[1])])
-    y_min = np.min([np.min(old[1]), np.min(new[1])])
-
-    # Create the first subplot
-    plt.subplot(2,1,1)
-    plt.plot(old[0],old[1], linestyle='None', marker=',')
-    plt.axis('equal')
-    plt.ylim([y_min, y_max])
-    plt.xlim([x_min, x_max])
-
-    # Create the second subplot
-    plt.subplot(2,1,2)
-    plt.plot(new[0], new[1],linestyle='None', marker=',')
-    plt.axis('equal')
-    plt.ylim([y_min, y_max])
-    plt.xlim([x_min, x_max])
-
-    # Display result
+    v = [-5,5,-5,5]
+    plt.subplot(1, 2, 1)
+    plt.title('Before')
+    plt.gca().set_aspect('equal')
+    plt.scatter(original[0], original[1])
+    plt.axis(v)
+    plt.subplot(1, 2, 2)
+    plt.title('After')
+    plt.gca().set_aspect('equal')
+    plt.scatter(new[0], new[1])
+    plt.axis(v)
     plt.show()
 
-
-def dilate(pts, stretch):
-    '''
-    Apply a dilation to an array of points. You should
-    implement the dilation as a matrix multiplication. Use np.diag to create
-    an appropriate matrix from the stretch argument.
-    Inputs:
-        pts -- a (2,n) array of points, x-coordinates on the first row, y-coordinates
-                on the second row.
-        stretch -- a length 2 NumPy array giving the dilation factor in the x- and
-                    y-direction entrywise, respectively
-    Returns:
-        a (2,n) array representing the dilated points.
-    '''
-    return np.dot(np.diag(stretch), pts)
-
-
-def rotate(pts, angle):
-    '''
-    Apply a rotation to an array of points. Inside of the function you will need to 
-    create the appropriate rotation matrix, and then perform the requisite matrix 
-    multiplication, and return the result.
-    Inputs:
-        pts -- a (2,n) array of points, x-coordinates on the first row, y-coordinates
-                on the second row.
-        angle -- a scalar value indicating the angle of rotation in radians.
-    Returns:
-        a (2,n) arrary representing the rotated points.
-    '''
-    a = math.cos(angle)
-    b = math.sin(angle)
-    rot = np.array([a,-b,b,a]).reshape((2,2))
-    return np.dot(rot, pts)
-
-
-def shear(pts, c, direction):
-    '''
-    Apply a shear to an array of points. You will need an if statement to deal with
-    the possible values of the direction argument.
-    Inputs:
-        pts -- a (2,n) array of points, x-coordinates on the first row, y-coordinates
-                on the second row.
-        c -- a scalar value indicating the amount of shearing
-        direction -- value of 0 means horizontal shear, 1 means vertical shear.
-    Returns:
-        a (2,n) array holding the sheared points.
-    '''
-    shear_mat = np.eye(2)
-    if direction==0:
-        shear_mat[0,1]=c
-    else:
-        shear_mat[1,0]=c
-    return np.dot(shear_mat,pts)
-
-
-def reflect(pts, axis):
-    '''
-    Apply a reflection to an array of points. You will need to use this array to create 
-    the appropriate reflection matrix, and then perform matrix multiplication.
-    Inputs:
-         pts -- a (2,n) array of points, x-coordinates on the first row, y-coordinates
-                on the second row.
-         axis -- a length two array representing a vector pointing in the direction 
-                  of the axis of rotation.
-    Returns:
-        a (2,n) array containing the reflected points.
-    '''
-    a = ((axis**2)*np.array([1,-1])).sum()
-    b = 2*axis.prod()
-    c = (axis**2).sum()
-    ref = (np.array([a,b,b,-a]).reshape((2,2)))/float(c)
-    return np.dot(ref,pts)
-
-
-def translate(pts, shift):
-    '''
-    Apply a translation to an array of points. To perform the translation, you simply 
-    need to add the shift array to the pts array, but before doing this, use the 
-    .reshape method on the shift array to ensure that it has the same shape as the 
-    example given in the text.
-    Inputs:
-        pts -- a (2,n) array of points, x-coordinates on the first row, y-coordinates
-                on the second row.
-        shift -- a length two array representing the amount of translation in each direction.
-    '''
-    return pts + shift.reshape((2,1))
-
-
-def trajectory(t,w,v,s):
-    '''
-    Calculate the trajectory of an object rotating about a movign point. 
-    Hint: you will use a composition of rotation and translation in this problem. 
-    Make sure the function returns an array indicating the new position of p1 at 
-    time t. 
-    Inputs:
-        t -- time (in seconds)
-        w -- angular velocity of object (radians/seconds)
-        v -- direction of the center of rotation
-        s -- speed of center of rotation (meters/second)
-    Returns:
-        the position of the rotating object at time t.
-    '''
-    angle = t*w
-    p = np.array([[0],[1]])
-    shift = ((s*t)/la.norm(v))*v
-    return translation(rotation(p, angle), shift)
-
-
-def plotTrajectory():
-    '''
-    Plot the trajectory of the rotating particle as calculated in the trajectory
-    function. 
-    '''
-    v = np.array([1, 1])
-    times = np.arange(0,10,.1)
-    pos = np.zeros((2,len(times)))
-    for i in range(len(times)):
-        pos[:,i] = trajectory(times[i],np.pi,v,3).flatten()
-    plt.plot(pos[0], pos[1])
-    plt.show()
+def dilation2D(array, x_factor, y_factor):
+    T = np.array([[x_factor,0],[0,y_factor]])
+    return T.dot(array)
     
+def rotate2D(array, theta):
+    T = np.array([[np.cos(theta),-np.sin(theta)],[np.sin(theta),np.cos(theta)]])
+    return T.dot(array)
+    
+def translate2D(array, b):
+    b = np.vstack([b[0],b[1]])
+    return array + b
+        
+def rotatingParticle(time,omega,direction,speed):
+    direction = np.array(direction)
+    T = np.linspace(time[0],time[1],100)
+    start_P1 = [1,0]
+    posP1_x = []
+    posP1_y = []
+    
+    for t in T:
+        posP2 = speed*t*direction/la.norm(direction)
+        posP1 = translate2D(rotate2D(start_P1, t*omega), posP2)[0]
+        posP1_x.append(posP1[0])
+        posP1_y.append(posP1[1])
+        
+    plt.plot(posP1_x, posP1_y)
+    plt.gca().set_aspect('equal')
+    plt.show()
+        
+def type_I(A, i, j):  
+    '''Swap two rows.'''
+    A[i], A[j] = np.copy(A[j]), np.copy(A[i])
+    
+def type_II(A, i, const):  
+    '''Multiply row i of A by const.'''
+    A[i] *= const
+    
+def type_III(A, i, j, const):  
+    '''Add a constant time row j to row i.'''
+    A[i] += const*A[j]
+    
+def REF(A):
+    A1 = np.copy(A)
+    
+    step = 1
+    for j in xrange(1,len(A1[0])):
+        step = j
+        for i in xrange(len(A1)-step):
+            A1[i+step,step-1:] -= (A1[i+step,step-1]/A1[step-1,step-1]) * A1[step-1,step-1:]
+    return A1
+    
+def LU(A):
+    U = np.copy(A)
+    L = np.identity(np.sqrt(A.size))
+    
+    for i in xrange(1,A.shape[0]):
+        for j in xrange(i):
+            L[i,j] = U[i,j]/U[j,j]
+            U[i,j:] -= L[i,j]*U[j,j:]
+    return U, L
+
+def time_LU():
+    A = np.random.random((1000,1000))
+    B = np.random.random((1000,500))
+    
+    before = time.time()
+    LU = la.lu_factor(A)
+    time_lu_factor = time.time() - before
+    
+    before = time.time()
+    A_inv = la.inv(A)
+    time_inv = time.time() - before
+    
+    before = time.time()
+    a = la.lu_solve(LU,B)
+    time_lu_solve = time.time() - before
+    
+    before = time.time()
+    b = A_inv.dot(B)
+    time_inv_solve = time.time() - before
+    
+    print "LU factor: " + str(time_lu_factor)
+    print "Inv: " + str(time_inv)
+    print "LU solve: " + str(time_lu_solve)
+    print "Inv solve: " + str(time_inv_solve)
--- Labs/AffineTransformations/spec.py
+++ Labs/AffineTransformations/spec.py
@@ -1,13 +1,10 @@
-# solutions.py
+# spec.py
 """Volume I Lab 5: Invertible Affine Transformations and Linear Systems.
-Name: Tanner Christensen
-Date: Fall 2015
+Name:
+Date:
 """
 
-import numpy as np
-import time
-from matplotlib import pyplot as plt
-from scipy import linalg as la
+# include your import statements here.
 
 # Helper Functions
 def plot_transform(original, new):
@@ -31,7 +28,7 @@ def plot_transform(original, new):
     plt.show()
 
 def type_I(A, i, j):  
-    """Swap the i-th and j-th rows."""
+    """Swap the i-th and j-th rows of A."""
     A[i], A[j] = np.copy(A[j]), np.copy(A[i])
     
 def type_II(A, i, const):  
@@ -39,45 +36,43 @@ def type_II(A, i, const):
     A[i] *= const
     
 def type_III(A, i, j, const):  
-    """Add a constant of the j-th to the i-th row."""
+    """Add a constant of the j-th row of A to the i-th row."""
     A[i] += const*A[j]
 
 
 # Problem 1
 def dilation2D(A, x_factor, y_factor):
     """Scale the points in A by x_factor in the x direction and y_factor in
-    the y direction.
+    the y direction. Returns the new array.
     
     Inputs:
         A (array) - Array of size (2,n) containing points in R2 stored as columns.
         x_factor (float) - scaling factor in the x direction.
         y_factor (float) - scaling factor in the y direction.
     """
-    T = np.array([[x_factor,0],[0,y_factor]])
-    return T.dot(A)
+    pass
     
 # Problem 2
 def rotate2D(A, theta):
-    """Rotate the points in A about the origin by theta radians.
+    """Rotate the points in A about the origin by theta radians. Returns 
+    the new array.
     
     Inputs:
         A (array) - Array of size (2,n) containing points in R2 stored as columns.
         theta (float) - number of radians to rotate points in A.
     """
-    T = np.array([[np.cos(theta),-np.sin(theta)],[np.sin(theta),np.cos(theta)]])
-    return T.dot(A)
+    pass
     
 # Problem 3
 def translate2D(A, b):
-    """Translate the points in A by the vector b.
+    """Translate the points in A by the vector b. Returns the new array.
     
     Inputs:
         A (array) - Array of size (2,n) containing points in R2 stored as columns.
         b (2-tuple (b1,b2)) - Translate points by b1 in the x direction and by b2 
             in the y direction.
     """
-    b = np.vstack([b[0],b[1]])
-    return A + b
+    pass
    
 # Problem 4
 def rotatingParticle(time, omega, direction, speed):
@@ -90,75 +85,34 @@ def rotatingParticle(time, omega, direction, speed):
      - direction (2-tuple (x,y)): Vector indicating direction.
      - speed (float): Distance per second.
     """
-    direction = np.array(direction)
-    T = np.linspace(time[0],time[1],100)
-    start_P1 = [1,0]
-    posP1_x = []
-    posP1_y = []
-    
-    for t in T:
-        posP2 = speed*t*direction/la.norm(direction)
-        posP1 = translate2D(rotate2D(start_P1, t*omega), posP2)[0]
-        posP1_x.append(posP1[0])
-        posP1_y.append(posP1[1])
-        
-    plt.plot(posP1_x, posP1_y)
-    plt.gca().set_aspect('equal')
-    plt.show()
+    pass
     
 # Problem 5
 def REF(A):
     """Reduce a square matrix A to REF. During a row operation, do not
     modify any entries that you know will be zero before and after the
-    operation."""
-    
-    A1 = np.copy(A)
-    
-    step = 1
-    for j in xrange(1,len(A1[0])):
-        step = j
-        for i in xrange(len(A1)-step):
-            A1[i+step,step-1:] -= (A1[i+step,step-1]/A1[step-1,step-1]) * A1[step-1,step-1:]
-    return A1
+    operation. Returns the new array."""
+    pass
     
 # Problem 6
 def LU(A):
     """Returns the LU decomposition of a square matrix."""
-    U = np.copy(A)
-    L = np.identity(np.sqrt(A.size))
-    
-    for i in xrange(1,A.shape[0]):
-        for j in xrange(i):
-            L[i,j] = U[i,j]/U[j,j]
-            U[i,j:] -= L[i,j]*U[j,j:]
-    return L,U
+    pass
 
 # Problem 7
 def time_LU():
     """Print the times it takes to solve a system of equations using
     LU decomposition and (A^-1)B where A is 1000x1000 and B is 1000x500."""
     
-    A = np.random.random((1000,1000))
-    B = np.random.random((1000,500))
     
-    before = time.time()
-    LU = la.lu_factor(A)
-    time_lu_factor = time.time() - before
-    
-    before = time.time()
-    A_inv = la.inv(A)
-    time_inv = time.time() - before
-    
-    before = time.time()
-    a = la.lu_solve(LU,B)
-    time_lu_solve = time.time() - before
-    
-    before = time.time()
-    b = A_inv.dot(B)
-    time_inv_solve = time.time() - before
+    time_lu_factor = 0  # set this to the time it takes to perform la.lu_factor(A)
+    time_inv = 0 # set this to the time it takes to take the inverse of A
+    time_lu_solve = 0  # set this to the time it takes to perform la.lu_solve()
+    time_inv_solve = 0  # set this to the time it take to perform (A^-1)B
+
 
     print "LU solve: " + str(time_lu_factor + time_lu_solve)
     print "Inv solve: " + str(time_inv + time_inv_solve)
     
     # What can you conclude about the more efficient way to solve linear systems?
-    print "For matrices of this size, LU decomposition is measureably faster."
+    print ""  # print your answer here.
