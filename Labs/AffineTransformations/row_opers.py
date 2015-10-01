b4268f25274d60b8073c8085ea8cb6073d7b4a9c
tkchris93 <tannerkchristensen@gmail.com>
2015-10-01 00:27:12 -0600

    fixing some consistency issues

--- Labs/AffineTransformations/row_opers.py
+++ Labs/AffineTransformations/row_opers.py
@@ -1,11 +1,11 @@
-def type_I(A, i, j):
-    '''Swap two rows.'''
+def type_I(A, i, j):  
+    """Swap the i-th and j-th rows of A."""
     A[i], A[j] = np.copy(A[j]), np.copy(A[i])
-
-def type_II(A, i, const):
-    '''Multiply row j of A by const.'''
+    
+def type_II(A, i, const):  
+    """Multiply the i-th row of A by const."""
     A[i] *= const
-
-def type_III(A, i, j, const):
-    '''Add a constant times row j to row i'''
+    
+def type_III(A, i, j, const):  
+    """Add a constant of the j-th row of A to the i-th row."""
     A[i] += const*A[j]
