315b663062d3ce79791f203f6b8ecab2c459ac53
Shane <shanemcq18@gmail.com>
2015-10-20 00:48:27 -0600

    Almost done with Markov

--- Labs/MarkovChains/solutions.py
+++ Labs/MarkovChains/solutions.py
@@ -1,157 +1,174 @@
-from __future__ import division
-import scipy as sp
-from scipy import linalg as la
-from scipy.linalg import hessenberg
+# solutions.py
+"""Volume II Lab 8: Markov Chains
+Solutions file. Written by Shane McQuarrie, even though
+it should have been written by Jared Webb. All well.
+"""
+
 import numpy as np
-from numpy.linalg import matrix_power
+from scipy.sparse import lil_matrix
+from os import system
 
-def qr_solver(A, niter=1000, tol=1e-3):
-    '''
-    Calculate the eigenvalues of a matrix A using QR iteration.
-    Inputs:
-        A -- real square matrix
-        niter -- number of QR iterations to run
-        tol -- real number. All real numbers with magnitude below we treat as 0
+
+# Problem 1: implement this function.
+def random_markov(n):
+    """Create a transition matrix for a random Markov chain with n states.
+    This should be stored as an nxn numpy array. The columns sum to 1.
+    """
+    transition_matrix = np.empty((n,n))
+    for j in range(n):
+        column = np.random.random(n)
+        column /= column.sum()
+        transition_matrix[:,j] = column
+    return transition_matrix
+
+
+# Problem 2: modify this function.
+def forecast(num_days):
+    """Run a simulation for the weather over 'num_days' days, with
+    'hot' as the starting state. Return a list containing the day-by-day
+    results, not including the starting day.
+
+    Example:
+        >>> forecast(3)
+        [1, 1, 0]
+
+        # Or, if you prefer,
+        >>> forecast(5)
+        ['cold', 'hot', 'hot', 'cold', 'cold']
+    """
+    transition_matrix = np.array([[.7, .6], [.3, .4]])
+    current_state = 0
+    record = []
+    for day in xrange(num_days):
+        random_number = np.random.random()
+        if random_number < transition_matrix[1, current_state]:
+            current_state = 1
+        else:
+            current_state = 0
+        record.append(current_state)
+    return record
+# Roughly 66.7% of the entries should be zeros.
+# Roughly 33.3% of the entries should be ones.
+
+
+# Problem 3: implement this function.
+def four_state_forecast(days=1):
+    transition = np.array(
+        [[.5, .3, .1, 0],[.3, .3, .3, .3],[.2, .3, .4, .5],[0, .1, .2, .1]])
+    current_state = 0
+    record = []
+    for day in xrange(days):
+        current_state = np.argmax(
+                    np.random.multinomial(1, transition[:,current_state]))
+        record.append(current_state)
+    return record
+# Roughly 24.6% of the entries should be zeros.
+# Roughly 30.1% of the entries should be ones.
+# Roughly 33.2% of the entries should be twos.
+# Roughly 12.1% of the entries should be threes.
+
+
+# Problem 4: implement this function.
+def analyze_simulation():
+    """Analyze the results of the previous two problems."""
+    hot1, cold1, hot2, mild, cold2, freezing  = [], [], [], [], [], []
+    for i in xrange(10):
+        f2 = forecast(10000)
+        f4 = four_state_forecast(10000)
+        hot1.append(f2.count(0))
+        cold1.append(f2.count(1))
+        hot2.append(f4.count(0))
+        mild.append(f4.count(1))
+        cold2.append(f4.count(2))
+        freezing.append(f4.count(3))
+    print("2-state forecast Hot days:\t%f%%"%(np.mean(hot1)/100.))
+    print("2-state forecast Cold days:\t%f%%"%(np.mean(cold1)/100.))
+    print("4-state forecast Hot days:\t%f%%"%(np.mean(hot2)/100.))
+    print("4-state forecast Mild days:\t%f%%"%(np.mean(mild)/100.))
+    print("4-state forecast Cold days:\t%f%%"%(np.mean(cold2)/100.))
+    print("4-state forecast Freezing days:\t%f%%"%(np.mean(freezing)/100.))
+
+
+def problem5(filename):
+    """Read in from a file, convert to ints, read out to a file."""
+
+    word_list = ['$tart']
+    with open(filename, 'r') as f:
+        contents = f.readlines()
+    outfile = open("int_file.txt", 'w')
+    for line in contents:
+        sentence = line.split()
+        for word in sentence:
+            if word not in word_list:
+                word_list.append(word)
+            outfile.write(str(word_list.index(word)) + " ")
+        outfile.write('\n')
+    word_list.append('en&')
+    return word_list
+
+
+def problem6(int_file, num_states, sparse=False):
+
+    # Initialize the transition matrix.
+    if sparse:
+        markov = lil_matrix((num_states, num_states))
+    else:
+        markov = np.zeros((num_states, num_states))
+
+    # Read in the data and process it.
+    with open(int_file, 'r') as f:
+        contents = f.readlines()
+    data = []
+    for line in contents:
+        data.append(line.split())
+
+    # Build the matrix.
+    for i in xrange(len(data)-1):
+        line = data[i]
+        markov[int(line[0]), 0] += 1
+        for j in xrange(len(line)-1):
+            markov[int(line[j+1]), int(line[j])] += 1
+        markov[num_states-1, int(line[-1])] += 1
+
+    # Divide by nonzero column sums.
+    for j in xrange(num_states):
+        s = markov[:,j].sum()
+        if s != 0:
+            markov[:,j] /= s
+    return markov
+
+def sentences(infile, outfile, num_sentences=1):
+    """Generate random sentences using the word list generated in
+    Problem 5 and the transition matrix generated in Problem 6.
+    Write the results to the specified outfile.
+
+    Parameters:
+        infile (str): The path to a filen containing a training set.
+        outfile (str): The file to write the random sentences to.
+        num_sentences (int): The number of random sentences to write.
+
     Returns:
-        eigs -- list of real or complex numbers, the eigenvalues of A.
-    Note: this code is far from optimized, but considering that the algorithm itself is
-          inherently limited in robustness, any extra optimization short of chaning the
-          algorithm seems wasted effort.
-    '''
-    # first calculate the upper hessenberg form of A
-    H = la.hessenberg(A)
-    
-    # perform the QR iterations
-    for i in xrange(niter):
-        Q, R = la.qr(H)
-        H = R.dot(Q)
-        
-    # populate the list of eigenvalues
-    eigs = []
-    i = 0
-    while i < H.shape[0]:
-        if i == H.shape[0]-1: #if we are at the last diagonal entry, must be eigenvalue
-            eigs.append(H[i,i])
-            i += 1
-        elif abs(H[i+1,i]) < tol: #if subdiagonal zero, diagonal must be e'value 
-            eigs.append(H[i,i])
-            i += 1
-        else: #subdiagonal nonzero, so find complex e'values of 2x2 block
-            a, b, c, d = H[i:i+2,i:i+2].flatten()
-            T = a + d
-            D = a*d - b*c
-            eigs.append(T/2 + math.sqrt(abs(T**2/4 - D))*1j)
-            eigs.append(T/2 - math.sqrt(abs(T**2/4 - D))*1j)
-            i += 2
-    return eigs
+        None
+    """
 
-def eig(A, normal = False, iter = 100):
-	'''Finds eigenvalues of an nxn array A. If A is normal, QRalg.eig 
-	may also return eigenvectors.
-	
-	Parameters
-	----------
-	A :  nxn array
-	     May be real or complex
-	normal : bool, optional
-		     Set to True if A is normal and you want to calculate
-		     the eigenvectors.
-	iter : positive integer, optional
-			
-	Returns
-	-------
-	v : 1xn array of eigenvectors, may be real or complex
-	Q : (only returned if normal = True) 
-		nxn array whose columns are eigenvectors, s.t. A*Q = Q*diag(v)
-		real if A is real, complex if A is complex
-	
-	For more on the QR algorithm, see Eigenvalue Solvers lab.
-	'''
-	def getSchurEig(A):
-		#Find the eigenvalues of a Schur form matrix. These are the 
-		#elements on the main diagonal, except where there's a 2x2 
-		#block on the main diagonal. Then we have to find the 
-		#eigenvalues of that block.
-		D = sp.diag(A).astype(complex)
-		#Find all the 2x2 blocks:
-		LD = sp.diag(A,-1)
-		index = sp.nonzero(abs(LD)>.01)[0] #is this a good tolerance?
-		#Find the eigenvalues of those blocks:
-		a = 1
-		b = -D[index]-D[index+1]
-		c = D[index]*D[index+1] - A[index,index+1]*LD[index]
-		discr = sp.sqrt(b**2-4*a*c)
-		#Fill in vector D with those eigenvalues
-		D[index] = (-b + discr)/(2*a)
-		D[index+1] = (-b - discr)/(2*a)
-		return D
+    # Get output from previous problems.
+    word_list = problem5(infile)
+    transition = problem6('int_file.txt', len(word_list), False)
 
-	n,n = A.shape
-	I = sp.eye(n)
-	A,Q = hessenberg(A,True)
-	if normal == False:
-		for i in sp.arange(iter):
-			s = A[n-1,n-1].copy()
-			Qi,R = la.qr(A-s*I)
-			A = sp.dot(R,Qi) + s*I
-		v = getSchurEig(A)
-		return v
-	
-	elif normal == True:
-		for i in sp.arange(iter):
-			s = A[n-1,n-1].copy()
-			Qi,R = la.qr(A-s*I)
-			A = sp.dot(R,Qi) + s*I
-			Q = sp.dot(Q,Qi)
-		v = sp.diag(A)
-		return v,Q
-		
-		
-def markov(transitions, initial_state, niter):
-    '''
-    You must write this method.
-    Given a transition matrix, calculate the state after a number of 
-    iterations using Markov chains, and the unique stable fixed point.
-    Inputs:
-        transitions -- a transition matrix (all positive values)
-        initial_state -- vector of the initial state
-        niter -- number of iterations
-    Returns:
-        cur_state -- the current state
-        stable_fixed_point -- the stable fixed point of the matrix
-    '''
-    pass
+    # Transition through the Markov chain.
+    stop = transition.shape[1] - 1
+    output = ""
+    for i in xrange(num_sentences):
+        current_state = 0
+        while current_state != stop:
+            current_state = np.argmax(
+                np.random.multinomial(1, transition[:,current_state]))
+            if current_state != stop:
+                output += word_list[current_state] + " "
+            else:
+                output += "\n"
 
-
-
-def markov_freethrow():
-    '''
-    Compute the answers to problem 1 in the lab.
-    Initialize the given Markov matrix, and perform all
-    calculations within the function.
-    Returns:
-        n1 -- n1 is the answer to part 1
-        n2 -- n2 is the answer to part 2
-    '''
-    A = np.array([[.75, .5], [.25, .5]])
-    n1 = A.dot(A)[0,0]
-    n2 = matrix_power(A, 20)[0,0]
-    return n1, n2
-
-def markov_stablept():
-    '''
-    Calculate the answers to problem 2 in the lab.
-    Hint: to see if there is a stable fixed point, raise the
-    matrix to some high powers (like powers of 15-20), and see
-    if the columns are converging to some common array. If
-    they are, return the common array, and if not, return None.
-    Returns:
-        n1 -- transition matrix asked for in part 1
-        n2 -- probability of being in state 2 after 2 
-              transitions (starting from state 1)
-        n3 -- array giving the stable fixed point if exists, or None
-    '''
-    A = np.array([[1./4, 1./3, 1./2], [1./4, 1./3, 1./3], [1./2, 1./3, 1./6]])
-    n2 = A.dot(A)[1,0]
-    n3 = matrix_power(A, 20)[:,0]
-    return A, n2, n3
+    # Write the results to the specified output file.
+    with open(outfile, 'w') as f:
+        f.write(output)
+    system("rm int_file.txt")
