83fc904068300e0129aeeb282dfbd2ab1cec46c2
Shane <shanemcq18@gmail.com>
2016-03-07 11:08:36 -0700

    working, mostly

--- Labs/InteriorPoint1/solutions.py
+++ Labs/InteriorPoint1/solutions.py
@@ -33,19 +33,13 @@ def startingPoint(A, b, c):
     return x, lam, mu
 
 # Problems 1-5
-def interiorPoint(A, b, c, niter=20):
+def interiorPoint(A, b, c, niter=20, tol=1e-16, verbose=False):
     """Interior Point, Baby!
     """
 
     m,n = A.shape
     def F(x_, l_, m_):
         return np.hstack((np.dot(A.T, l_) + m_ - c, np.dot(A, x_) - b, m_*x_))
-
-    # def DF(x_, m_):
-    #     top = np.hstack((np.zeros((n,n)), A.T, np.eye(n)))
-    #     mid = np.hstack((A, np.zeros((m,m)), np.zeros((m,n))))
-    #     low = np.hstack((np.diag(m_), np.zeros((n,m)), np.diag(x_)))
-    #     return np.row_stack((top, mid, low))
 
     DF = np.zeros((2*n+m, 2*n+m))
     DF[:n,n:-n] = A.T
@@ -58,25 +52,24 @@ def interiorPoint(A, b, c, niter=20):
     assert len(lam) == len(b) == m
 
     e = np.ones_like(mu)
-    sigma = .1
+    sigma = .5
 
-    for i in xrange(niter):
+    i = 0
+    nu = 1.
+    while i < niter:# and nu >= tol:
+        i += 1
 
+        # Problem 3: Search Direction
         DF[-n:,:n] = np.diag(mu)
         DF[-n:,-n:] = np.diag(x)
 
-        # Problem 3: Search Direction
         nu = np.dot(x, mu) / n
         nu_vec = np.hstack((np.zeros(n+m), e*nu*sigma))
         lu_piv = la.lu_factor(DF)
-        direct = la.lu_solve(lu_piv, F(x,lam,mu))
-        # direct = la.solve(DF(x,mu), nu_vec - F(x,lam,mu))
+        direct = la.lu_solve(lu_piv, nu_vec - F(x,lam,mu))
 
         # Problem 4: Step Length
         trix, trilam, trimu = direct[:n], direct[n:-n], direct[-n:]
-        print trix, trilam, trimu
-        print x, lam, mu
-        raw_input()
         mask = trimu < 0
         if np.any(mask):
             alpha = min(1, -(mu/trimu)[mask].min())
@@ -88,26 +81,56 @@ def interiorPoint(A, b, c, niter=20):
         else:
             delta = 1
 
+        # Problem 5: Finish it up.
         x += delta*trix
         lam += alpha*trilam
         mu += alpha*trimu
 
-        print(nu)
-
+        if verbose:
+            print("Interation {:0>2} nu = {}".format(i+1, nu))
+    if i < niter:
+        print("Converged in {} iterations".format(i))
     return x, c.dot(x)
 
+def randomLP2(m):
+    """Generate a linear program min c^T x s.t. Ax = b, x>=0.
+    First generate m feasible constraints, then add slack variables
+    to convert it into the above form.
+    Inputs:
+        m -- positive integer >= n, number of desired constraints.
+        n -- dimension of space in which to optimize.
+    Outputs:
+        A -- array of shape (m,n).
+        b -- array of shape (m,).
+        c -- array of shape (n,).
+        x -- the solution to the LP.
+    """
+    n = m
+    A = np.random.random((m,n))*20 - 10
+    A[A[:,-1]<0] *= -1
+    x = np.random.random(n)*10
+    b = A.dot(x)
+    c = A.sum(axis=0)/n
+    return A, b, -c, x
 
+
+def test_interiorPoint(m=7, n=5):
+    from IntPointSolutions import randomLP
+    A, b, c, v = randomLP(m, n)
+    point, value = interiorPoint(A, b, c)
+    assert np.allclose(v, point[:5]), "FAILED:\nv: {}\n\npt: {}".format(v, point[:5])
+
+    # Test with CVXOPT
+    # from cvxopt import matrix, solvers
+    # A, b, c = matrix(A), matrix(b), matrix(c)
+    # G = matrix(-np.eye(5))
+    # h = matrix(np.zeros(5))
+    # sol = solvers.lp(c, G, h, A, b)
+    # assert np.allclose(point, np.array(sol["x"]).flatten()), "{}\n\n{}".format(point, sol["x"])
+    # assert np.isclose(value, sol["primal objective"]), "{} {}".format(value, sol["primal objective"])
 
 if __name__ == '__main__':
-    # A = np.array([[1, -1.1],[3.1,  1],[4.2,  3]], dtype=np.float)
-    # b = np.array([2, 5, 7], dtype=np.float)
-    # c = np.array([3, 2], dtype=np.float)
-
-    from IntPointSolutions import randomLP
-    A, b, c, v = randomLP(3,2)
-    A = A[:,:2]
-    c = c[:2]
-    print A, b, c; raw_input()
-    interiorPoint(A, b, c)
-
+    for i in xrange(1000):
+        print '\r{}'.format(i),
+        test_interiorPoint()
 
