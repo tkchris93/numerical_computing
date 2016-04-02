fa886443d0cadf687d95c9f25036d63544b3d6d8
Ben Ehlert <ben@ehlert.org>
2016-04-02 17:26:30 -0600

    Added the problem 2 to a spec file

---
+++ Labs/Pseudospectra/spec.py
@@ -0,0 +1,16 @@
+# Problem 2
+def ps_contour_plot(A, m = 20,epsilon_vals=None):
+    '''Plots the pseudospectrum of the matrix A as a contour plot.  Also,
+    plots the eigenvalues.
+    Parameters:
+        A : square, 2D ndarray
+            The matrix whose pseudospectrum is to be plotted
+        m : int
+            accuracy
+        epsilon_vals : list of floats
+            If k is in epsilon_vals, then the epsilon-pseudospectrum
+            is plotted for epsilon=10**-k
+            If epsilon_vals=None, the defaults of plt.contour() are used
+            instead of any specified values.
+    '''
+    raise NotImplementedError("Problem 2 Incomplete")
