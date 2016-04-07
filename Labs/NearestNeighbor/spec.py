0971eae08f801b8b9199091e62c3a2550399caf5
Shane <shanemcq18@gmail.com>
2016-04-07 00:44:25 -0600

    updated KDT spec

--- Labs/NearestNeighbor/spec.py
+++ Labs/NearestNeighbor/spec.py
@@ -1,27 +1,39 @@
-# name this file solutions.py
-"""Volume II Lab 6: Nearest Neighbor Search
+# Name this file 'solutions.py'.
+"""Volume II: Nearest Neighbor Search.
 <Name>
 <Class>
 <Date>
 """
 
-from Trees import BST
-
+import numpy as np
 
 # Problem 1: Implement this function.
-def euclidean_metric(x, y):
-    """Return the euclidean distance between the vectors 'x' and 'y'.
+def metric(x, y):
+    """Return the euclidean distance between the 1-D arrays 'x' and 'y'.
 
     Raises:
-        ValueError: if the two vectors 'x' and 'y' are of different lengths.
+        ValueError: if 'x' and 'y' have different lengths.
     
     Example:
-        >>> print(euclidean_metric([1,2],[2,2]))
+        >>> metric([1,2],[2,2])
         1.0
-        >>> print(euclidean_metric([1,2,1],[2,2]))
+        >>> metric([1,2,1],[2,2])
         ValueError: Incompatible dimensions.
     """
-    raise NotImplementedError("Problem 1 Incomplete")
+    if len(x) != len(y):
+        raise ValueError("Incompatible dimensions")
+    return np.linalg.norm(x - y)
+    
+    # Or a slightly longer way:
+    return np.sqrt(np.sum((x - y)**2))
+    # Or the longest/worst way:
+    total = 0
+    for i in xrange(len(x)):
+        term = x[i] - y[i]
+        total += term**2
+    total = np.sqrt(total)
+    return total
+
 
 # Problem 2: Implement this function.
 def exhaustive_search(data_set, target):
@@ -37,102 +49,102 @@ def exhaustive_search(data_set, target):
         the member of 'data_set' that is nearest to 'target' (1xk ndarray).
         The distance from the nearest neighbor to 'target' (float).
     """
-    raise NotImplementedError("Problem 2 Incomplete")
+
+    # Initialize the outputs.
+    minimum_distance = float("inf")
+    nearest_neighbor = None
+
+    # Search through the entire data set for the nearest neighbor.
+    for point in data_set:
+        distance = metric(target, point)
+        if distance < minimum_distance:
+            nearest_neighbor = point
+            minimum_distance = distance
+
+    return nearest_neighbor, minimum_distance
 
 
-# Problem 3: Finish implementing this class by modifying __init__()
-#   and adding the __sub__, __eq__, __lt__, and __gt__ magic methods.
-class KDTNode(BSTNode):
-    """Node class for K-D Trees. Inherits from BSTNode.
-
-    Attributes:
-        left (KDTNode): a reference to this node's left child.
-        right (KDTNode): a reference to this node's right child.
-        parent (KDTNode): a reference to this node's parent node.
-        data (ndarray): a coordinate in k-dimensional space.
-        axis (int): the 'dimension' of the node to make comparisons on.
-    """
-
-    def __init__(self, data):
-        """Construct a K-D Tree node containing 'data'. The left, right,
-        and prev attributes are set in the constructor of BSTNode.
-
-        Raises:
-            TypeError: if 'data' is not a a numpy array (of type np.ndarray).
-        """
-        BSTNode.__init__(self, data)
-        self.axis  = 0
+# Problem 3: Write a KDTNode class.
 
 
 # Problem 4: Finish implementing this class by overriding
-#   the insert() and remove() methods.
+#            the __init__(), insert(), and remove() methods.
 class KDT(BST):
     """A k-dimensional binary search tree object.
     Used to solve the nearest neighbor problem efficiently.
 
     Attributes:
         root (KDTNode): the root node of the tree. Like all other
-            nodes in the tree, the root houses data as a numpy array.
+            nodes in the tree, the root houses data as a NumPy array.
         k (int): the dimension of the tree (the 'k' of the k-d tree).
     """
     
     def find(self, data):
-        """Return the node containing 'data'.
-
-        Raises:
-            ValueError: if there is node containing 'data' in the tree,
-                or the tree is empty.
+        """Return the node containing 'data'. If there is no such node
+        in the tree, or if the tree is empty, raise a ValueError.
         """
 
-        # First check that the tree is not empty.
-        if self.root is None:
-            raise ValueError(str(data) + " is not in the tree.")
-        
         # Define a recursive function to traverse the tree.
-        def _step(current, target):
-            """Recursively approach the target node."""
-            
-            if current is None:             # Base case: target not found.
-                return current
-            if current == other:            # Base case: target found!
-                return current
-            if target < current:            # Recursively search to the left.
-                return _step(current.left, target)
-            else:                           # Recursively search to the right.
-                return _step(current.right, target)
-            
-        # Create a new node to use the KDTNode comparison operators.
-        n = KDTNode(data)
+        def _step(current):
+            """Recursively step through the tree until the node containing
+            'data' is found. If there is no such node, raise a Value Error.
+            """
+            if current is None:                     # Base case 1: dead end.
+                raise ValueError(str(data) + " is not in the tree")
+            elif np.allclose(data, current.value):
+                return current                      # Base case 2: data found!
+            elif data[current.axis] < current.value[current.axis]:
+                return _step(current.left)          # Recursively search left.
+            else:
+                return _step(current.right)         # Recursively search right.
+        
+        # Start the recursion on the root of the tree.
+        return _step(self.root)
+    
+    def insert(self, data):
+        """Insert a new node containing 'data' at the appropriate location.
+        Return the new node. This method should be similar to BST.insert().
+        """
+        raise NotImplementedError("Problem 4 Incomplete")
 
-        # Call the recursive function, starting at the root.
-        found = _step(self.root, n)
-        if found is None:                  # Report the data was not found.
-            raise ValueError(str(data) + " is not in the tree.")
-        return found                       # Otherwise, return the target node.
 
-
-# Problem 5: Implement this function.
+# Problem 5: Implement this function
 def nearest_neighbor(data_set, target):
-    """Use the KDT class to solve the nearest neighbor problem.
+    """Use your KDTree class to solve the nearest neighbor problem.
 
     Inputs:
-        data_set (mxk ndarray): An array of m k-dimensional points.
-        target (1xk ndarray): A k-dimensional point to compare to 'dataset'.
+        data_set ((m,k) ndarray): An array of m k-dimensional points.
+        target ((k,) ndarray): A k-dimensional point to compare to 'dataset'.
 
     Returns:
-        The point in the tree that is nearest to 'target' (1xk ndarray).
+        The point in the tree that is nearest to 'target' ((k,) ndarray).
         The distance from the nearest neighbor to 'target' (float).
     """
+    
+    def KDTsearch(current, neighbor, distance):
+        """The actual nearest neighbor search algorithm.
+        
+        Inputs:
+            current (KDTNode): the node to examine.
+            neighbor (KDTNode): the current nearest neighbor.
+            distance (float): the current minimum distance.
+
+        Returns:
+            neighbor (KDTNode): The new nearest neighbor in the tree.
+            distance (float): the new minimum distance.
+        """
+        pass
+
     raise NotImplementedError("Problem 5 Incomplete")
 
 
-# Problem 6: Implement this function.
 def postal_problem():
     """Use the neighbors module in sklearn to classify the Postal data set
     provided in 'PostalData.npz'. Classify the testpoints with 'n_neighbors'
     as 1, 4, or 10, and with 'weights' as 'uniform' or 'distance'. For each
     trial print a report indicating how the classifier performs in terms of
-    percentage of misclassifications.
+    percentage of correct classifications. Which combination gives the most
+    correct classifications?
 
     Your function should print a report similar to the following:
     n_neighbors = 1, weights = 'distance':  0.903
@@ -140,4 +152,5 @@ def postal_problem():
     """
     raise NotImplementedError("Problem 6 Incomplete")
 
-# =============================== END OF FILE =============================== #
+
+# End of File =================================================================
