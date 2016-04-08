b200274d4f44ea2fdce4d4cde9a61db97efd90ea
Shane <shanemcq18@gmail.com>
2016-04-07 21:09:56 -0600

    fixed KDT test driver

--- Labs/NearestNeighbor/testDriver.py
+++ Labs/NearestNeighbor/testDriver.py
@@ -8,7 +8,7 @@
 import inspect
 import numpy as np
 from scipy.spatial import KDTree
-from new_solutions import metric, postal_problem
+from solutions import metric, postal_problem
 
 
 def test(student_module):
@@ -37,81 +37,79 @@ class _testDriver(object):
     def __init__(self):
         self.feedback = ""
 
-    # Main routine ------------------------------------------------------------
-    def test_all(self, student_module):
+    # Main routine -----------------------------------------------------------
+    def test_all(self, student_module, total=50):
+        """Grade the provided module on each problem and compile feedback."""
+        # Reset feedback and score.
         self.feedback = ""
-        score = 0
+        self.score = 0
 
-        try:    # Problem 1: 5 points
-            self.feedback += "\n\nProblem 1 (5 points):"
-            points = self.problem1(student_module)
-            score += points
-            self.feedback += "\nScore += " + str(points)
-        except BaseException as e:
-            self.feedback += "\nError: " + e.message
-        
-        try:    # Problem 2: 5 points
-            self.feedback += "\n\nProblem 2 (5 points):"
-            points = self.problem2(student_module)
-            score += points
-            self.feedback += "\nScore += " + str(points)
-        except BaseException as e:
-            self.feedback += "\nError: " + e.message
+        def test_one(problem, label, value):
+            """Test a single problem, checking for errors."""
+            try:
+                self.feedback += "\n\n{} ({} points):".format(label, value)
+                points = problem(student_module)
+                self.score += points
+                self.feedback += "\nScore += {}".format(points)
+            except BaseException as e:
+                self.feedback += "\n{}: {}".format(self._errType(e), e)
 
-        try:    # Problems 4 and 5: 30 points
-            self.feedback += "\n\nProblems 4 and 5 (30 points):"
-            points = self.problem5(student_module)
-            score += points
-            self.feedback += "\nScore += " + str(points)
-        except BaseException as e:
-            self.feedback += "\nError: " + e.message
+        # Grade each problem.
+        test_one(self.problem1, "Problem 1", 5)
+        test_one(self.problem2, "Problem 2", 5)
+        test_one(self.problem5, "Problems 4 and 5", 30)
+        test_one(self.problem6, "Problem 6", 10)
 
-        try:    # Problem 6: 10 points
-            self.feedback += "\n\nProblem 6 (10 points):"
-            # points = self.problem6(student_module)
-            score += points
-            self.feedback += "\nScore += " + str(points)
-        except BaseException as e:
-            self.feedback += "\nError: " + e.message
-        
         # Report final score.
-        total = 50
-        perc = (100. * score) / total
-        self.feedback += "\n\nTotal score: %d/%d = %s%%"%(score, total, perc)
-        if   perc >=  98.0: self.feedback += "\n\nExcellent!"
-        elif perc >=  90.0: self.feedback += "\n\nGreat job!"
+        percentage = (100. * self.score) / total
+        self.feedback += "\n\nTotal score: {}/{} = {}%".format(
+                                    self.score, total, round(percentage, 2))
+        if   percentage >=  98: self.feedback += "\n\nExcellent!"
+        elif percentage >=  90: self.feedback += "\n\nGreat job!"
 
         # Add comments (optionally).
-        print self.feedback
+        print(self.feedback)
         comments = str(raw_input("Comments: "))
         if len(comments) > 0:
-            self.feedback += '\n\n\nComments:\n\t' + comments
-        self.score = score
-        
-    # Helper functions --------------------------------------------------------
-    def numTest(self, correct, student, message):
-        """Test to see if correct and student are numerically close.
-        If not, provide feedback. Return 1 for correct and 0 otherwise.
+            self.feedback += '\n\n\nComments:\n\t{}'.format(comments)
+
+
+    # Helper Functions --------------------------------------------------------
+    @staticmethod
+    def _errType(error):
+        """Get just the name of the exception 'error' in string format."""
+        return str(type(error).__name__)
+
+    def _eqTest(self, correct, student, message):
+        """Test to see if 'correct' and 'student' are equal.
+        Report the given 'message' if they are not.
         """
         if np.allclose(correct, student, atol=1e-04):
             return 1
         else:
-            self.feedback += message
-            self.feedback += "\nCorrect response:\n" + str(correct)
-            self.feedback += "\nStudent response:\n" + str(student)
+            self.feedback += "\n{}".format(message)
+            self.feedback += "\n\tCorrect response: {}".format(correct)
+            self.feedback += "\n\tStudent response: {}".format(student)
             return 0
 
-    def grade(self, points):
-        """Manually grade a problem out of 'points'. Return the points earned.
+    def _grade(self, points, message=None):
+        """Manually grade a problem worth 'points'. Return the score.
+        If full points are not earned, get feedback on the problem.
         """
         credit = -1
         while credit > points or credit < 0:
             try:
-                credit = int(input("\nScore out of " + str(points) + ": "))
+                credit = int(input("\nScore out of {}: ".format(points)))
             except:
                 credit = -1
         if credit != points:
-            self.feedback += "\n\t" + str(raw_input("Describe problem: "))
+            # Add comments (optionally),
+            comments = raw_input("Comments: ")
+            if len(comments) > 0:
+                self.feedback += "\n{}".format(comments)
+            # Or add a predetermined error message.
+            elif message is not None:
+                self.feedback += "\n{}".format(message)
         return credit
 
     def neighbor(self, m, k, func):
@@ -124,9 +122,9 @@ class _testDriver(object):
         dist, index = tree.query(target)
         point = tree.data[index]
         spoint, sdist = func(data, target) # func solves the problem
-        p1 = self.numTest(point, spoint,
+        p1 = self._eqTest(point, spoint,
             "\n\t"+func.__name__+"() failed: incorrect nearest neighbor")
-        p2 = self.numTest(dist, sdist, 
+        p2 = self._eqTest(dist, sdist, 
             "\n\t"+func.__name__+"() failed: incorrect minimum distance")
         return p1 + p2
 
@@ -143,17 +141,17 @@ class _testDriver(object):
         # Test with good inputs (4 points)
         x = np.array([1, 2])
         y = np.array([2, 2])
-        points = self.numTest(metric(x,y), s.metric(x,y),
+        points = self._eqTest(metric(x,y), s.metric(x,y),
                                             "\n\tmetric() failed.")
         
         x = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
         y = np.array([2, 6, 4, 8, 0, 2, 4, 7, 5, 11])
-        points += self.numTest(metric(x,y), s.metric(x,y),
+        points += self._eqTest(metric(x,y), s.metric(x,y),
                                             "\n\tmetric() failed.")
         
         x = (np.random.random(100)-.5)*200
         y = (np.random.random(100)-.5)*200
-        points += self.numTest(metric(x,y), s.metric(x,y),
+        points += self._eqTest(metric(x,y), s.metric(x,y),
                                         "\n\tmetric() failed.")*2
         
         # Test with bad inputs (1 point)
@@ -177,7 +175,7 @@ class _testDriver(object):
 
         _testDriver.get_code(s.exhaustive_search)
         print "\n(Check that scipy.spatial.KDTree is not used)"
-        points *= self.grade(1)
+        points *= self._grade(1)
 
         return points
 
@@ -198,7 +196,7 @@ class _testDriver(object):
         x = np.random.random(10); y = np.random.random(10)
         A =   KDTNode(x); B =   KDTNode(y)
         C = s.KDTNode(x); D = s.KDTNode(y)
-        points += 2*self.numTest(A-B, C-D, "\n\tKDTNode.__sub__ failed")
+        points += 2*self._eqTest(A-B, C-D, "\n\tKDTNode.__sub__ failed")
 
         # Test KDTNode.__eq__ (1 Point)
         D = s.KDTNode(1.5*x)
@@ -235,7 +233,7 @@ class _testDriver(object):
 
         _testDriver.get_code(s.nearest_neighbor)
         print "\n(Check that scipy.spatial.KDTree is not used)"
-        points *= self.grade(1)
+        points *= self._grade(1)
         
         return points
 
@@ -249,10 +247,10 @@ class _testDriver(object):
         if x is not None:
             print x
         
-        return self.grade(10)
+        return self._grade(10)
 
 if __name__ == '__main__':
-    import new_solutions as sol
+    import solutions as sol
     score, feedback = test(sol)
 
 # =============================== END OF FILE =============================== #
