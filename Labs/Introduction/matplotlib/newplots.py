4da4e5f8fadb169ab2eeb55f968b95cd5e27fa35
Shane <shanemcq18@gmail.com>
2016-04-26 23:02:15 -0600

    mpl fixed domain explanation

---
+++ Labs/Introduction/matplotlib/newplots.py
@@ -0,0 +1,38 @@
+# newplots.py
+"""Introductory Labs: Matploblib and Mayavi. Plotting file."""
+
+import numpy as np
+from matplotlib import pyplot as plt
+# from mayavi import mlab
+
+from functools import wraps
+def _clear(func):
+    """Decorator for clearing and closing figures automatically."""
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            plt.clf()
+            return func(*args, **kwargs)
+        finally:
+            plt.clf()
+            plt.close('all')
+    return wrapper
+
+
+@_clear
+def basic1():
+    y = np.array([i**2 for i in xrange(-5,6)])
+    plt.plot(y)
+    plt.savefig("basic1.pdf", format='pdf')
+
+@_clear
+def basic2():
+    x = np.linspace(-5, 5, 50)
+    y = x**2
+    plt.plot(x, y)
+    plt.savefig("basic2.pdf", format='pdf')
+
+
+if __name__ == '__main__':
+    basic1()
+    basic2()
---
+++ Labs/Introduction/matplotlib/newsolutions.py
@@ -0,0 +1,16 @@
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+def prob1():
+    def var_of_means(n):
+        A = np.random.randn(n,n)
+        return A.mean(axis=1).var()
+
+    y = np.array([var_of_means(n) for n in xrange(100, 1100, 100)])
+    plt.plot(y)
+    plt.show()
+
+if __name__ == '__main__':
+    prob1()
+    
