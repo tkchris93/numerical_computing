2be10bafc4af8c12baff48b0655240f34c8dadc8
Mylan Cook <mylan.cook@gmail.com>
2016-05-31 10:49:39 -0600

    Added solutions.

--- Labs/Vol3A/PandasIII/plots.py
+++ Labs/Vol3A/PandasIII/plots.py
@@ -18,10 +18,32 @@ def gend_class():
 
 def piv_gend_class():
     tab = titanic.pivot_table('survived', index='sex', columns='class')
-    # save tab as piv_gend_class.pdf()
-    
+    # save tab as piv_gend_class.pdf
+
+def piv_gend_class_age():
+    tab = titanic.pivot_table('survived', index =['sex', age], columns='class')
+    # save tab as piv_gend_class_age.pdf
+
+def piv_gca_count():
+    tab = titanic.pivot_table('survived', columns='sex', index=['class',age], aggfunc='count')
+    # save tab as piv_gca_count.pdf
+
+def fare_splice():
+    fare = pd.qcut(titanic['fare'], 2)
+    tab = titanic.pivot_table('survived', index=['sex',age], columns=[fare, 'class'])
+    # save tab as fare_splice.pdf
+
+def fill_zeros():
+    fare = pd.qcut(titanic['fare'], 2)
+    tab = titanic.pivot_table('survived', index=['sex',age], columns=[fare, 'class'], fill_value=0.0)
+    # save tab as fill_zeros.pdf
+
 if __name__=='__main__':
     heading()
     gend_surv()
     gend_class()
     piv_gend_class()
+    piv_gend_class_age()
+    piv_gca_count()
+    fare_splice()
+    fill_zeros()
