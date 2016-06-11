1e4b8c35780a0bb07452da6ee218fda5e06bfd98
Shane <shanemcq18@gmail.com>
2016-06-10 21:48:16 -0600

    Update deleteBranch.sh

--- deleteBranch.sh
+++ deleteBranch.sh
@@ -3,9 +3,9 @@
 set -e                              # Exit as soon as one command fails.
 
 if [ "$#" -ne 1 ]; then             # Check for a command line argument.
-    echo "Provide the name of the branch to be deleted"
+    echo -e "Provide the name of the branch to be deleted"
 else                                # Delete the specified git branch.
-    echo "\nAttempting to delete branch $1...\n"
+    echo -e "\nAttempting to delete branch $1...\n"
     git checkout develop
     git pull upstream develop
     git checkout $1
