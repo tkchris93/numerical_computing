6f1f57af927d6a1b63ea835795bb8cf4d01135e9
Mylan Cook <mylan.cook@gmail.com>
2016-06-22 17:56:54 -0600

    Lotsa changes...

--- Labs/Vol3A/PandasIII/solutions.py
+++ Labs/Vol3A/PandasIII/solutions.py
@@ -1,8 +1,14 @@
 import pandas as pd
 
-titanic = pd.read_csv("titanic.csv")
-print titanic.head()
+titanic = pd.read_csv("titanic.csv")[['Pclass','Survived','Sex','Age','Fare']]
+titanic['Age'].fillna(titanic['Age'].mean())
+titanic.dropna(inplace=True)
+#print titanic.head()
 
 tab = titanic.pivot_table('Survived', index='Sex', columns='Pclass',aggfunc='count')
-print tab
-
+#print tab
+tab2 = titanic.pivot_table('Survived', index='Sex', columns='Pclass')
+#print tab2
+age = pd.cut(titanic['Age'], [0,12,18,80])
+tab3 = titanic.pivot_table('Survived',index =['Sex', age], columns='Pclass',aggfunc='count')
+print tab3
