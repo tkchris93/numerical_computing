c896db6c360eea314a9998e3969d2a5fe1a4a70d
Shane <shanemcq18@gmail.com>
2016-06-25 19:28:47 -0600

    linear systems beginning

---
+++ Labs/Vol1A/LinearSystems/row_opers.py
@@ -0,0 +1,11 @@
+def type_I(A, i, j):  
+    """Swap the i-th and j-th rows of A."""
+    A[i], A[j] = np.copy(A[j]), np.copy(A[i])
+    
+def type_II(A, i, const):  
+    """Multiply the i-th row of A by const."""
+    A[i] *= const
+    
+def type_III(A, i, j, const):  
+    """Add a constant of the j-th row of A to the i-th row."""
+    A[i] += const*A[j]
