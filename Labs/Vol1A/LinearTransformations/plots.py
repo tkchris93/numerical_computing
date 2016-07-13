0f2f4be40f8ce635f7ca046fdd08cb2d7c9fbc36
Shane <shanemcq18@gmail.com>
2016-07-13 10:24:54 -0600

    lin plots -> figures/

--- Labs/Vol1A/LinearTransformations/plots.py
+++ Labs/Vol1A/LinearTransformations/plots.py
@@ -13,6 +13,7 @@ from sys import stdout
 from time import time
 import numpy as np
 import solutions
+import os
 
 def _save(filename):
     """Decorator for saving, clearing, and closing figures automatically."""
@@ -22,134 +23,32 @@ def _save(filename):
         raise ValueError("Invalid file name '{}'".format(filename))
     if extension not in {"pdf", "png"}:
         raise ValueError("Invalid file extension '{}'".format(extension))
+    if not os.path.isdir("figures"):
+        os.mkdir("figures")
+
 
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
             try:
-                print("{:.<20}".format(func.__name__+'()'), end='')
+                print("{:.<40}".format(filename), end='')
                 stdout.flush()
                 plt.clf()
                 out = func(*args, **kwargs)
-                if extension == "pdf":
-                    plt.savefig(filename, format='pdf')
-                elif extension == "png":
-                    plt.savefig(filename, format='png')
+                plt.savefig("figures/"+filename, format=extension)
                 print("done.")
                 return out
             except Exception as e:
-                print("\n", e, sep='')
+                print("\n\t", e, sep='')
             finally:
                 plt.clf()
                 plt.close('all')
         return wrapper
     return decorator
 
-# Timing Pictures =============================================================
-
-@_save("time_random_matrix2.pdf")
-def timing_demo(N=12):
-    """Generate the two figures used just before Problem 1."""
-
-    domain = 2**np.arange(1,N+1)
-    times = []
-    for n in domain:
-        start = time()
-        solutions.random_matrix(n)
-        times.append(time() - start)
-
-    plt.plot(domain, times, 'g.-', linewidth=2, markersize=15)
-    plt.xlabel("n", fontsize=14)
-    plt.ylabel("Seconds", fontsize=14)
-    plt.savefig("time_random_matrix1.pdf", format="pdf")
-
-    # Plot the dotted blue line over the green line.
-    def f(x, a, b):
-        return a*x**2 + b
-    a, b = curve_fit(f, domain, times)[0]
-
-    refinement = np.linspace(1, 2**N, 200)
-    plt.plot(refinement, f(refinement, a, b), 'b--', lw=5)
-    plt.ylim(ymin=0)
-    plt.ylabel("Seconds", fontsize=14, color="white")
-
-@_save("matrixMatrixMultiplication.pdf")
-def prob1_solution(N=9):
-    """Generate the two figures used at the end of Problem 1."""
-
-    domain = 2**np.arange(1,N+1)
-    vector_times, matrix_times = [], []
-
-    for n in domain:
-        A = solutions.random_matrix(n)
-        x = solutions.random_vector(n)
-        B = solutions.random_matrix(n)
-
-        start = time()
-        solutions.matrix_vector_product(A, x)
-        vector_times.append(time() - start)
-
-        start = time()
-        solutions.matrix_matrix_product(A, B)
-        matrix_times.append(time() - start)
-
-    # Matrix-Vector Multiplication.
-    plt.plot(domain, vector_times, 'b.-', lw=2, ms=15)
-    plt.xlabel("n", fontsize=14); plt.ylabel("Seconds", fontsize=14)
-    plt.title("Matrix-Vector Multiplication")
-
-    plt.savefig("matrixVectorMultiplication.pdf", format="pdf")
-    plt.clf()
-    plt.close()
-
-    # Matrix-Matrix Multiplication.
-    plt.plot(domain, matrix_times, 'g.-', lw=2, ms=15)
-    plt.xlabel("n", fontsize=14); plt.ylabel("Seconds", fontsize=14, color="w")
-    plt.title("Matrix-Matrix Multiplication")
-
-    return domain, vector_times, matrix_times
-
-@_save("loglogDemoGood.pdf")
-def loglog_demo(domain, vector_times, matrix_times):
-    """Generate the two figures between Problem 1 and Problem 2."""
-
-    plt.plot(domain, vector_times, 'b.-', lw=2, ms=15, label="Matrix-Vector")
-    plt.plot(domain, matrix_times, 'g.-', lw=2, ms=15, label="Matrix-Matrix")
-    plt.legend(loc="upper left")
-    plt.savefig("loglogDemoBad.pdf", format="pdf")
-    plt.clf()
-    plt.close("all")
-
-    plt.loglog(domain, vector_times, 'b.-', basex=2, basey=2, lw=2, ms=15)
-    plt.loglog(domain, matrix_times, 'g.-', basex=2, basey=2, lw=2, ms=15)
-
-@_save("cachingDemo.pdf")
-def caching_demo():
-
-    sizes = 2**np.arange(2,19,.2)
-    times = []
-    for size in sizes:
-        A, B = np.random.random((2, size))
-        def func():
-            A + B
-        times.append(timeit(func, number=50) / 50.)
-
-    plt.loglog(sizes[1:], times[1:], basex=2, basey=2, lw=2, ms=15)
-    plt.ylabel("Seconds")
-    plt.xlabel("n")
-
-
-def timing_drawings():
-    timing_demo(12)
-    a, b, c = prob1_solution(8)
-    loglog_demo(a, b, c)
-    caching_demo()
-
 # Horse Pictures ==============================================================
 
 horse = np.load("horse.npy")[:,::10]
-
-# TODO: Get rid of tick marks.
 
 def save_horse(data, title, top=True):
     plt.clf()
@@ -165,7 +64,7 @@ def save_horse(data, title, top=True):
         labelbottom='off', labelleft='off')
     plt.axis([-1,1,-1,1])
     plt.gca().set_aspect("equal")
-    plt.savefig("{}Horse.pdf".format(title), format="pdf")
+    plt.savefig("figures/{}Horse.pdf".format(title), format="pdf")
     plt.clf()
     plt.close("all")
 
@@ -254,17 +153,117 @@ def solar_system(T=3*np.pi/2, omega_e=1, omega_m=13, N=400):
 def horse_drawings():
     for func in [original_horse, stretched_horse, sheared_horse, rotated_horse,
                  reflected_horse, combo_horse, translated_horse]:
-        print("{:.<20}".format(func.__name__+'()'), end='')
+        print("{:.<40}".format(func.__name__+'()'), end='')
         stdout.flush()
         func()
         print("done.")
     solar_system()
 
+# Timing Pictures =============================================================
+
+@_save("time_random_matrix2.pdf")
+def timing_demo(N=12):
+    """Generate the two figures used just before Problem 1."""
+
+    domain = 2**np.arange(1,N+1)
+    times = []
+    for n in domain:
+        start = time()
+        solutions.random_matrix(n)
+        times.append(time() - start)
+
+    plt.plot(domain, times, 'g.-', linewidth=2, markersize=15)
+    plt.xlabel("n", fontsize=14)
+    plt.ylabel("Seconds", fontsize=14)
+    plt.savefig("figures/time_random_matrix1.pdf", format="pdf")
+
+    # Plot the dotted blue line over the green line.
+    def f(x, a, b):
+        return a*x**2 + b
+    a, b = curve_fit(f, domain, times)[0]
+
+    refinement = np.linspace(1, 2**N, 200)
+    plt.plot(refinement, f(refinement, a, b), 'b--', lw=5)
+    plt.ylim(ymin=0)
+    plt.ylabel("Seconds", fontsize=14, color="white")
+
+@_save("matrixMatrixMultiplication.pdf")
+def prob1_solution(N=9):
+    """Generate the two figures used at the end of Problem 1."""
+
+    domain = 2**np.arange(1,N+1)
+    vector_times, matrix_times = [], []
+
+    for n in domain:
+        A = solutions.random_matrix(n)
+        x = solutions.random_vector(n)
+        B = solutions.random_matrix(n)
+
+        start = time()
+        solutions.matrix_vector_product(A, x)
+        vector_times.append(time() - start)
+
+        start = time()
+        solutions.matrix_matrix_product(A, B)
+        matrix_times.append(time() - start)
+
+    # Matrix-Vector Multiplication.
+    plt.plot(domain, vector_times, 'b.-', lw=2, ms=15)
+    plt.xlabel("n", fontsize=14); plt.ylabel("Seconds", fontsize=14)
+    plt.title("Matrix-Vector Multiplication")
+
+    plt.savefig("figures/matrixVectorMultiplication.pdf", format="pdf")
+    plt.clf()
+    plt.close()
+
+    # Matrix-Matrix Multiplication.
+    plt.plot(domain, matrix_times, 'g.-', lw=2, ms=15)
+    plt.xlabel("n", fontsize=14); plt.ylabel("Seconds", fontsize=14, color="w")
+    plt.title("Matrix-Matrix Multiplication")
+
+    return domain, vector_times, matrix_times
+
+@_save("loglogDemoGood.pdf")
+def loglog_demo(domain, vector_times, matrix_times):
+    """Generate the two figures between Problem 1 and Problem 2."""
+
+    plt.plot(domain, vector_times, 'b.-', lw=2, ms=15, label="Matrix-Vector")
+    plt.plot(domain, matrix_times, 'g.-', lw=2, ms=15, label="Matrix-Matrix")
+    plt.legend(loc="upper left")
+    plt.savefig("figures/loglogDemoBad.pdf", format="pdf")
+    plt.clf()
+    plt.close("all")
+
+    plt.loglog(domain, vector_times, 'b.-', basex=2, basey=2, lw=2, ms=15)
+    plt.loglog(domain, matrix_times, 'g.-', basex=2, basey=2, lw=2, ms=15)
+
+@_save("cachingDemo.pdf")
+def caching_demo():
+
+    sizes = 2**np.arange(2,19,.2)
+    times = []
+    for size in sizes:
+        A, B = np.random.random((2, size))
+        def func():
+            A + B
+        times.append(timeit(func, number=50) / 50.)
+
+    plt.loglog(sizes[1:], times[1:], basex=2, basey=2, lw=2, ms=15)
+    plt.ylabel("Seconds")
+    plt.xlabel("n")
+
+
+def timing_drawings():
+    timing_demo(12)
+    a, b, c = prob1_solution(8)
+    loglog_demo(a, b, c)
+    caching_demo()
+
 # =============================================================================
 
 def draw_all():
-    timing_drawings()
     horse_drawings()
+    timing_drawings()
 
 if __name__ == "__main__":
     draw_all()
