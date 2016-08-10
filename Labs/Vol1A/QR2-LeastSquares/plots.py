08c8f33748f31e45025d2f6d64eb9ff9d70361d3
Shane <shanemcq18@gmail.com>
2016-08-10 12:04:22 -0600

    qr2 new linregess problem

---
+++ Labs/Vol1A/QR2-LeastSquares/plots.py
@@ -0,0 +1,63 @@
+# plots.py
+"""Volume I: QR 2 (Least Squares and Eigenvalues). Plotting file."""
+from __future__ import print_function
+import matplotlib
+matplotlib.rcParams = matplotlib.rc_params_from_file('../../../matplotlibrc')
+
+from matplotlib import pyplot as plt
+from functools import wraps
+from sys import stdout
+import os
+
+def _save(filename):
+    """Decorator for saving, clearing, and closing figures automatically."""
+    try:
+        name, extension = filename.split(".")
+    except (ValueError, TypeError) as e:
+        raise ValueError("Invalid file name '{}'".format(filename))
+    if extension not in {"pdf", "png"}:
+        raise ValueError("Invalid file extension '{}'".format(extension))
+    if not os.path.isdir("figures"):
+        os.mkdir("figures")
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                print("{:.<40}".format(filename), end='')
+                stdout.flush()
+                plt.clf()
+                out = func(*args, **kwargs)
+                plt.savefig("figures/"+filename, format=extension)
+                print("done.")
+                return out
+            except Exception as e:
+                print("\n\t", e, sep='')
+            finally:
+                plt.clf()
+                plt.close('all')
+        return wrapper
+    return decorator
+
+# Figures =====================================================================
+
+import numpy as np
+from scipy.stats import linregress
+
+@_save("line_fit_example.pdf")
+def line():
+    x = np.linspace(0, 10, 20)
+    y = .5*x -3 + np.random.randn(20)
+    m, b = linregress(x, y)[:2]
+
+    plt.plot(x, y, 'k*', label="Data Points")
+    plt.plot(x, m*x + b, 'b-', lw=2, label="Least Squares Fit")
+    plt.legend(loc="upper left")
+
+# =============================================================================
+
+def draw_all():
+    line()
+
+if __name__ == "__main__":
+    draw_all()
--- Labs/Vol1A/QR2-LeastSquares/solutions.py
+++ Labs/Vol1A/QR2-LeastSquares/solutions.py
@@ -7,64 +7,99 @@ from matplotlib import pyplot as plt
 from numpy.lib import scimath as sm
 
 # Problem 1
-def least_squares(A,b):
-    """Return the least squares solutions to Ax = b using QR decomposition."""
+def least_squares(A, b):
+    """Calculate the least squares solutions to Ax = b using QR decomposition.
+
+    Inputs:
+        A ((m,n) ndarray): A matrix of rank n <= m.
+        b ((m, ) ndarray): A vector of length m.
+
+    Returns:
+        x ((n, ) ndarray): The solution to the normal equation.
+    """
     Q,R = la.qr(A, mode="economic")
-    return la.solve_triangular(R, np.dot(Q.T,b))
+    return la.solve_triangular(R, np.dot(Q.T, b))
 
-# Problem 2
+    # Or do back substitution by hand:
+    m,n = A.shape
+    Q,R = la.qr(A, mode="economic")
+    y = np.dot(Q.T, b)
+    x = np.zeros(n)
+
+    for k in reversed(range(n)):
+        x[k] = (y[k] - np.dot(R[k,k:], x[k:])) / R[k,k]
+
+    return x
+
+
+# Problem 2 with MLB DATA
 def line_fit():
-    """Plot linepts and its best-fit line on the same plot."""
-    linepts = np.load('data.npz')['linepts']
+    """Load the data from MLB.npy. Calculate the least squares solution to the
+    line that relates height to weight.
 
-    A = np.copy(linepts)
-    A[:,1] = 1
+    Plot the the original data points and the least squares line together.
+    """
+    height, weight, age = np.load("MLB.npy").T
+    A = np.column_stack((height, np.ones_like(height)))
+    # Or use slicing:
+    # A = np.ones((len(height), 2))
+    # A[:,0] = height
+    slope, intercept = least_squares(A, weight)
 
-    B = np.copy(linepts[:,1])
-    x_hat = la.lstsq(A,B)[0,0]
+    x = np.linspace(height.min()-2, height.max()+2, 20)
+    y = x*slope + intercept
 
-    x0 = np.linspace(0,4000,100)
-    y0 = x_hat*x0
-    plt.plot(A[:,:1], B, '.' , x0, y0)
-    plt.axis([0,4000,0,20000])
+    plt.plot(height, weight, '.')
+    plt.plot(x, y, 'k-', lw=2)
+    plt.show()
+
+
+def plot_ellipse(a, b, c, d, e):
+    """Plot (and show) an ellipse of the form ax^2 + bx + cxy + dy + ey^2 = 1.
+
+    Parameters:
+        a, b, c, d, e (floats): Coefficients from the equation of an ellipse
+                                of the form ax^2 + bx + cxy + dy + ey^2 = 1.
+    """
+    theta = np.linspace(0, 2*np.pi, 200)
+    cos_t, sin_t = np.cos(theta), np.sin(theta)
+    A = a*(cos_t**2) + c*cos_t*sin_t + e*(sin_t**2)
+    B = b*cos_t + d*sin_t
+    r = (-B + np.sqrt(B**2 + 4*A))/(2*A)
+
+    plt.plot(r*cos_t, r*sin_t)
+    plt.gca().set_aspect("equal", "datalim")
     plt.show()
 
 # Problem 3
 def ellipse_fit():
-    """Plot ellipsepts and its best-fit line on the same plot."""
-    ellipsepts = np.load('data.npz')['ellipsepts']
+    """Load the data from ellipse_pts.npy. Calculate the least squares
+    solution to the ellipse that best fits the points.
+
+    Plot the original data points and the least squares ellipse together.
+    """
+    ellipsepts = np.load("ellipse_pts.npy")#'data.npz')['ellipsepts']
     x_pts = ellipsepts[:,:1]
     y_pts = ellipsepts[:,1:]
     A = np.hstack((x_pts**2, x_pts, x_pts*y_pts, y_pts, y_pts**2))
     b = np.vstack((np.ones(len(ellipsepts))))
     a, b, c, d, e = la.lstsq(A,b)[0]
-    plot_ellipse(x_pts, y_pts, a, b, c, d, e)
+    plt.plot(x_pts, y_pts, 'k*')
+    plot_ellipse(a, b, c, d, e)
 
-def plot_ellipse(X, Y, a, b, c, d, e):
-    """Plots an ellipse of the form ax^2 + bx + cxy + dy + ey^2 = 1.
-
-    Input:
-      X (array) - x-coordinates of all the data points.
-      Y (array) - y-coordinates of all the data points.
-      a,b,c,d,e (float) - the coefficients from the equation of an
-                    ellipse of the form ax^2 + bx + cxy + dy + ey^2 = 1.
-    """
-    def get_r(a, b, c, d, e):
-        theta = np.linspace(0,2*np.pi,200)
-        A = a*(np.cos(theta)**2) + c*np.cos(theta)*np.sin(theta) + e*(np.sin(theta)**2)
-        B = b*np.cos(theta) + d*np.sin(theta)
-        r = (-B + np.sqrt(B**2 + 4*A))/(2*A)
-        return r, theta
-
-    r, theta = get_r(a,b,c,d,e)
-    plt.plot(r*np.cos(theta), r*np.sin(theta), color = "r")
-    plt.plot(X, Y)
-    plt.gca().set_aspect("equal", "datalim")
-    plt.show()
 
 # Problem 4
 def power_method(A, tol):
-    """Return the dominant eigenvalue of A and its corresponding eigenvector."""
+    """Compute the dominant eigenvalue of A and its corresponding eigenvector.
+
+    Inputs:
+        A ((n,n) ndarray): A square matrix.
+        tol (float): The stopping tolerance.
+
+    Returns:
+        eigval (foat): The dominant eigenvalue of A.
+        eigvec ((n, ) ndarray): The eigenvector corresponding to 'eigval'.
+    """
     size = A.shape[0]
     random_vector = []
     for i in xrange(size):
