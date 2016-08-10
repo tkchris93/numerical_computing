5d076b08c13ae02aab8f5047cd8f2753a70ef863
Shane <shanemcq18@gmail.com>
2016-08-09 22:35:39 -0600

    qr2 initial cleanup + prob1

--- Labs/Vol1A/QR2-LeastSquares/solutions.py
+++ Labs/Vol1A/QR2-LeastSquares/solutions.py
@@ -1,20 +1,22 @@
-#solutions.py
-from matplotlib import pyplot as plt
-from scipy import linalg as la
+# solutions.py
+"""Volume I: QR 2 (Applications). Solutions file."""
+
 import numpy as np
+from scipy import linalg as la
+from matplotlib import pyplot as plt
 from numpy.lib import scimath as sm
 
 # Problem 1
 def least_squares(A,b):
     """Return the least squares solutions to Ax = b using QR decomposition."""
-    Q,R = la.qr(A)
+    Q,R = la.qr(A, mode="economic")
     return la.solve_triangular(R, np.dot(Q.T,b))
 
 # Problem 2
 def line_fit():
     """Plot linepts and its best-fit line on the same plot."""
     linepts = np.load('data.npz')['linepts']
-    
+
     A = np.copy(linepts)
     A[:,1] = 1
 
@@ -23,7 +25,7 @@ def line_fit():
 
     x0 = np.linspace(0,4000,100)
     y0 = x_hat*x0
-    plt.plot(A[:,:1],B,'.',x0,y0)
+    plt.plot(A[:,:1], B, '.' , x0, y0)
     plt.axis([0,4000,0,20000])
     plt.show()
 
@@ -35,8 +37,8 @@ def ellipse_fit():
     y_pts = ellipsepts[:,1:]
     A = np.hstack((x_pts**2, x_pts, x_pts*y_pts, y_pts, y_pts**2))
     b = np.vstack((np.ones(len(ellipsepts))))
-    a,b,c,d,e = la.lstsq(A,b)[0]
-    plot_ellipse(x_pts,y_pts,a,b,c,d,e)
+    a, b, c, d, e = la.lstsq(A,b)[0]
+    plot_ellipse(x_pts, y_pts, a, b, c, d, e)
 
 def plot_ellipse(X, Y, a, b, c, d, e):
     """Plots an ellipse of the form ax^2 + bx + cxy + dy + ey^2 = 1.
@@ -44,7 +46,7 @@ def plot_ellipse(X, Y, a, b, c, d, e):
     Input:
       X (array) - x-coordinates of all the data points.
       Y (array) - y-coordinates of all the data points.
-      a,b,c,d,e (float) - the coefficients from the equation of an 
+      a,b,c,d,e (float) - the coefficients from the equation of an
                     ellipse of the form ax^2 + bx + cxy + dy + ey^2 = 1.
     """
     def get_r(a, b, c, d, e):
@@ -53,15 +55,15 @@ def plot_ellipse(X, Y, a, b, c, d, e):
         B = b*np.cos(theta) + d*np.sin(theta)
         r = (-B + np.sqrt(B**2 + 4*A))/(2*A)
         return r, theta
-        
-    r,theta = get_r(a,b,c,d,e)
+
+    r, theta = get_r(a,b,c,d,e)
     plt.plot(r*np.cos(theta), r*np.sin(theta), color = "r")
-    plt.plot(X,Y,".", color = "b")
-    plt.axes().set_aspect('equal', 'datalim')
+    plt.plot(X, Y)
+    plt.gca().set_aspect("equal", "datalim")
     plt.show()
 
 # Problem 4
-def power_method(A,tol):
+def power_method(A, tol):
     """Return the dominant eigenvalue of A and its corresponding eigenvector."""
     size = A.shape[0]
     random_vector = []
@@ -70,20 +72,20 @@ def power_method(A,tol):
     norm = la.norm(random_vector)
     random_vector = np.array(random_vector, dtype = np.float)
     random_vector /= norm
-    
+
     while True:
         product = np.dot(A,random_vector)
-        new_vector = product/la.norm(product)
+        new_vector = product / la.norm(product)
         if (la.norm(new_vector - random_vector) < tol):
-            break  
+            break
         random_vector = new_vector
-    
+
     eigenvector = random_vector
     eigenvalue = np.inner(np.dot(A,eigenvector),eigenvector)/la.norm(eigenvector)
     return eigenvalue, eigenvector
-    
+
 # Problem 5
-def QR_algorithm(A,niter,tol):
+def QR_algorithm(A, niter, tol):
     """Return the eigenvalues of A using the QR algorithm."""
     H = la.hessenberg(A)
     for i in xrange(niter):
@@ -107,7 +109,7 @@ def QR_algorithm(A,niter,tol):
             C = a*d-b*c
             eigen_plus = (-B + sm.sqrt(B**2 - 4*C))/2.
             eigen_minus = (-B - sm.sqrt(B**2 - 4*C))/2.
-            
+
             eigenvalues.append(eigen_plus)
             eigenvalues.append(eigen_minus)
             i+=1
