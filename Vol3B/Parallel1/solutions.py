015ec64e12b79bf25498f02c854d91102d853445
tkchris93 <tannerkchristensen@gmail.com>
2016-11-09 23:11:46 -0700

    finished solutions file

--- Vol3B/Parallel1/solutions.py
+++ Vol3B/Parallel1/solutions.py
@@ -31,7 +31,6 @@ def prob4(N):
     def draw(N):
         import numpy as np
         a = np.random.randn(N, m).astype(np.float32)
-        print a.shape
         maxs = a.max(axis=1)
         return maxs
 
@@ -42,5 +41,51 @@ def prob4(N):
     return myarray
 
 def prob5():
-    # solution coming.
-    # Create a dictionary on each engine, gather the dictionaries, then create the sparse occurence matrix.
+    def create_occurrence_dict():
+        occur_dict = dict()
+        for doc in batch:
+            processed_words = []
+
+            # get a list of words
+            with open(doc, 'r') as f:
+                lines = f.read()
+            words = lines.strip().lower().split(' ')
+            for w in words:
+                if w not in stopwords_set:
+                    processed_words.append(w)
+
+            occur_dict[doc] = dict()
+            for w in processed_words:
+                if w not in occur_dict[doc].keys():
+                    occur_dict[doc][w] = 1
+                else:
+                    occur_dict[doc][w] += 1
+
+        return occur_dict
+
+    from nltk.corpus import stopwords
+    from os import walk
+    import scipy.sparse as spar
+
+    stopwords_set = set(stopwords.words('english'))
+    stopwords_set.add("")
+
+    for (dirname, dirpath, filename) in walk("state_union"):
+        doc_batch = ["/home/tanner/Work/DevTeam/numerical_computing/Vol3B/Parallel1/state_union/" + f for f in filename]
+
+    dview.scatter("batch", doc_batch)
+    dictionaries = dview.apply_sync(create_occurrence_dict)
+
+    vocab = []
+    for d in dictionaries:
+        for doc in d.keys():
+            vocab += d[doc].keys()
+
+    vocab = sorted(list(set(vocab)))
+
+    occur_matrix = spar.csr_matrix((len(filename), len(vocab))).tolil()
+    for d in dictionaries:
+        for i, doc in enumerate(sorted(d.keys())):
+            for j, word in enumerate(sorted(d[doc].keys())):
+                occur_matrix[i,j] += d[doc][word]
+    return occur_matrix.tocsr()
