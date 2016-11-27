23610771b0cb25128ac8fcbae9a6df357287beb4
Shane <shanemcq18@gmail.com>
2016-11-26 23:01:55 -0700

    gausquad solutions and spec updated SO MUCH

--- Vol2A/GaussianQuadrature/solutions.py
+++ Vol2A/GaussianQuadrature/solutions.py
@@ -1,153 +1,133 @@
-#gaussquad16.py
-'''
-Shane McQuarrie		Math 347	22 January 2015
-Lab 16: Gaussian Quadrature
-'''
+# solutions.py
+"""Lab 16: Gaussian Quadrature. Solutions file."""
 
 import numpy as np
-from matplotlib import pyplot as plt
-from math import sqrt, exp
-from scipy.linalg import eig
+from math import sqrt
+from scipy import stats
+from scipy import linalg as la
 from scipy.integrate import quad
-from scipy.stats import norm
+from matplotlib import pyplot as plt
 
-# ============================== PROBLEM 1 ============================== #
 
-# Testing the integral (numerically) of the function x**2 and its shift.
-def shift_test():
-	def g(x):
-		return (9.0/4.0)*x**2 + (15.0/2.0)*x + (25.0/4.0)
-	def G(x):
-		return (9.0/8.0)*x**3 + (45.0/8.0)*x**2 + (75.0/8.0)*x
-	a = G(1)-G(-1)
-	b = (4**3)/3.0 - 1/3.0
-	if np.allclose(a,b):
-		print "Success! G(1)-G(-1) = integral of x^2 from 1 to 4 = ",a
-		return True
-	else:
-		print "FAILURE! a = ",a,", b = ",b
-		return False
+# Problem 1
+def shift(f, a, b, plot=False):
+    """Shift the function f on [a, b] to a new function g on [-1, 1] such that
+    the integral of f from a to b is equal to the integral of g from -1 to 1.
 
-# function shifting problem
-def shift_function(f, a, b):
-	'''
-	'f' is a callable function, 'a' and 'b' are
-	the limits of the interval.
-	Return the shifted function g.
-	'''
-	g = lambda x: f(((b-a)/2.)*x + (b+a)/2.)
-	return g
+    Inputs:
+        f (function): a scalar-valued function on the reals.
+        a (int): the left endpoint of the interval of integration.
+        b (int): the right endpoint of the interval of integration.
+        plot (bool): if True, plot f over [a,b] and g over [-1,1] in separate
+            subplots.
 
-# plotting for the example in the function shifting problem
-def funcplot(f, a, b, n=401):
-	'''
-	Constructs and plots the example given in the
-	problem on shifting the domain of a function to [-1, 1].
-	'n' is the number of points to use to generate the plot.
+    Returns:
+        The new, shifted function.
+    """
+    # Define g.
+    g = lambda x: f((b - a)*x/2. + (a + b)/2.)
 
-	This def should plot f on [a,b] and the shifted function
-	(b-a)/2*g on [-1,1]
-	'''
-	g = shift_function(f,a,b)
-	X = np.linspace(a,b,n)
-	plt.subplot(121)
-	plt.plot(X,f(X),label='f(x)')
-	plt.title('f(x)')
-	plt.subplot(122)
-	Y = np.linspace(-1,1,n)
-	plt.plot(Y,g(Y)*(b-a)/2.0)
-	plt.title('(b-a)g(x)/2')
-	plt.suptitle('Function Shifting')
-	plt.show()
+    if plot is True:
 
-# ============================== PROBLEM 2 ============================== #
+        plt.subplot(121)    # Plot f(x) over [a,b]
+        x1 = np.linspace(a, b, 200)
+        plt.plot(x1, f(x1), 'b-', lw=2)
+        plt.title(r"$f(x)$ on $[{}, {}]$".format(a,b))
 
-# integral estimation problem
+        plt.subplot(122)    # Plot g(x) over [-1,1]
+        x2 = np.linspace(-1, 1, 200)
+        plt.plot(x2, g(x2), 'g-', lw=2)
+        plt.title(r"$g(x)$ on $[-1, 1]$")
+
+        plt.show()
+
+    return g
+
+
+# Problem 2
 def estimate_integral(f, a, b, points, weights):
-	'''
-	Estimate the value of an integral given
-	the function 'f', the interval bounds 'a' and 'b',
-	the nodes to use for sampling, and their
-	corresponding weights.
+    """Estimate the value of the integral of the function f over [a,b].
 
-	Return:
-	The value of the integral
-	'''
-	g = shift_function(f,a,b)
-	return ((b-a)/2.) * np.inner(weights, g(points))
+    Inputs:
+        f (function): a scalar-valued function on the reals.
+        a (int): the left endpoint of the interval of integration.
+        b (int): the right endpoint of the interval of integration.
+        points ((n,) ndarray): an array of n sample points.
+        weights ((n,) ndarray): an array of n weights.
 
-# ============================== PROBLEM 3 ============================== #
+    Returns:
+        The approximate integral of f over [a,b].
+    """
+    return (b - a) * np.dot(weights, shift(f, a, b, False)(points)) / 2.
 
-# Jacobi construction problem
+
+# Problem 3
 def construct_jacobi(gamma, alpha, beta):
-	'''
-	Construct the Jacobi matrix given the
-	sequences 'alpha', 'beta', and 'gamma' from the 
-	three term recurrence relation.
+    """Construct the Jacobi matrix."""
+    a = - beta / alpha
+    b = np.sqrt(gamma[1:] / (alpha[:-1] * alpha[1:]))
 
-	Return:
-	The Jacobi matrix
-	'''
+    J = np.diag(a)
+    B = np.diag(b)
+    J[:-1,1:] += B
+    J[1:,:-1] += B
 
-	''' First attempt
-	n = len(alpha)
-	jac = np.zeros((n,n))
-	b = np.sqrt(gamma[1]/(alpha[0]*alpha[1])) #initial b
-	for i in xrange(n):
-		jac[i,i] = -beta[0]/alpha[0] # diagonal
-		if i > 0: jac[i,i-1] = b #subdiagonal
-		if i < n-1:
-			jac[i,i+1] = b #superdiagonal
-			b = sqrt(gamma[i+1]/(alpha[i]*alpha[i+1]))
-	return jac
-	''' # Faster attempt
-	n = len(alpha)
-	jac = np.zeros((n,n))
-	a = - beta / alpha
-	b = np.sqrt(gamma[1:]/(alpha[:-1]*alpha[1:]))
-	np.fill_diagonal(jac,a)
-	np.fill_diagonal(jac[:,1:], b)
-	np.fill_diagonal(jac[1:,:], b)
-	return jac
+    return J
 
-# ============================== PROBLEM 4 ============================== #
 
-# points and weights problem
-def points_and_weights(n,p=False):
-	'''
-	Find the set of 'n' nodes and their
-	corresponding weights for the interval [-1, 1].
+# Problem 4
+def points_and_weights(n):
+    """Calculate the points and weights for a quadrature over [a,b] with n
+    points.
 
-	Return:
-	The points and weights used to calculate the integral of the Legendre polynomials.
-	Using scipy.linalg.eig(A) can help find eigenvalues and eigenvectors of A.
-	'''
-	alpha = np.zeros(n)
-	beta = np.zeros(n)
-	gamma = np.zeros(n)
-	for i in np.arange(1,n+1,dtype='float'):#xrange(len(X)):
-		alpha[i-1] = (2.*i - 1)/i
-		beta[i-1] = 0
-		gamma[i-1] = (i - 1)/i
-	jac = construct_jacobi(gamma, alpha, beta)
-	if p: print X,alpha,beta,gamma,jac
-	evals, evecs = eig(jac)
-	print evecs
-	weights = [(evecs[i,0]**2)*2 for i in xrange(evecs.shape[0])]
-	return evals, weights
+    Returns:
+        points ((n,) ndarray): an array of n sample points.
+        weights ((n,) ndarray): an array of n weights.
+    """
 
-# ============================== PROBLEM 5 ============================== #
+    # Calculate the recurrence coefficients.
+    gamma = np.array([(k-1)/float(k) for k in xrange(1,n+1)])
+    alpha = np.array([(2*k - 1)/float(k) for k in xrange(1,n+1)])
+    beta = np.zeros(n)
 
-# normal distribution cdf problem
-def normal_cdf(x,p=False):
-	'''
-	Compute the CDF of the standard normal
-	distribution at the point 'x'.
-	'''
-	f = lambda z: (1./np.sqrt(2.*np.pi))*np.e**((-z**2)/2.)
-	answer = quad(f,-5,x)[0]
-	if p and np.allclose(answer,norm.cdf(x)): print "Success!"
-	return answer
+    # Get the eigenvalues of the Jacobian.
+    J = construct_jacobi(gamma, alpha, beta)
+    eigs, vecs = la.eig(J)
+
+    # Get the points and weights from the Jacobi eigenvalues and eigenvectors.
+    points = eigs
+    weights = 2 * vecs[0]**2
+
+    return points.real, weights.real
+
+    evals, evecs = eig(jac)
+    print evecs
+    weights = [(evecs[i,0]**2)*2 for i in xrange(evecs.shape[0])]
+    return evals, weights
+
+
+# Problem 5
+def gaussian_quadrature(f, a, b, n):
+    """Using the functions from the previous problems, integrate the function
+    'f' over the domain [a,b] using 'n' points in the quadrature.
+    """
+    points, weights = points_and_weights(n)
+    return estimate_integral(f, a, b, points, weights)
+
+    evals, evecs = eig(jac)
+    print evecs
+    weights = [(evecs[i,0]**2)*2 for i in xrange(evecs.shape[0])]
+    return evals, weights
+
+
+# Problem 6
+def normal_cdf(x):
+    """Use scipy.integrate.quad() to compute the CDF of the standard normal
+    distribution at the point 'x'. That is, compute P(X <= x), where X is a
+    normally distributed random variable with mean = 0 and std deviation = 1.
+    """
+    return quad(norm.pdf, -5, x)[0]
+
 
 # ============================== END OF FILE ============================== #
 
@@ -156,14 +136,14 @@ from numpy.random import randn
 # Test script
 def test(student_module):
     """Test script. Import the student's solutions file as a module.
-    
+
      5 points for problem 2
     30 points for problem 6
      5 points for problem 7
-    
+
     Inputs:
         student_module: the imported module for the student's file.
-    
+
     Returns:
         score (int): the student's score, out of TOTAL.
         feedback (str): a printout of test results for the student.
@@ -267,18 +247,18 @@ class _testDriver(object):
 
     # Problems ----------------------------------------------------------------
     @_autoclose
-    def problem2(self, s):
-        """Test shift_example(). 5 points."""
-        s.shift_example()
-        return self._grade(5, "shift_example() failed")
+    def problem1(self, s):
+        """Test shift(). 5 points."""
+        s.prob2()
+        return self._grade(5, "prob2() failed")
 
-    def problem6(self, s):
-        """Test gaussian_quadrature(). 30 points."""
+    def problem5(self, s):
+        """Test gaussian_quadrature(). 30 points.""" # LOL spread it out...
         f = lambda x: x**2
         points =  10*self._eqTest(21, s.gaussian_quadrature(f, 1, 4, 5),
                             "gaussian_quadrature() failed for f(x) = "
                             "x^2 over [1, 4]")
-        
+
         g = lambda x: 4*x**3 - 3*x**2 + 2*x - 5
         points += 10*self._eqTest(46, s.gaussian_quadrature(g, 2, 3, 6),
                             "gaussian_quadrature() failed for f(x) = "
@@ -287,14 +267,14 @@ class _testDriver(object):
         h = lambda x: (x-5)**3
         points += 10*self._eqTest(0, s.gaussian_quadrature(h, 3, 7, 5),
                             "gaussian_quadrature() failed for f(x) = "
-                            "x^2 - 2x + 1 over [3, 7]")
+                            "(x-5)^3 over [3, 7]")
         return points
 
-    def problem7(self, s):
+    def problem6(self, s):
         """Test normal_cdf(). 5 points."""
         points  = 2*self._eqTest(norm.cdf(1), s.normal_cdf(1),
                                         "normal_cdf(1) failed")
         x = randn()
         points += 3*self._eqTest(norm.cdf(x), s.normal_cdf(x),
-                                        "normal_cdf(%f) failed"%x)
+                                        "normal_cdf({!f}) failed".format(x))
         return points
