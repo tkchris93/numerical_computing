f5667055c37ca9d996d8457cfab84d47fc03430b
Spencer Giddens <giddens2spencer@gmail.com>
2016-12-07 14:19:09 -0700

    Updated wording of problems to make more clear what the outputs should be. Updated solutions file to look cleaner and to use functions. Created spec file called web_scraping1.py

---
+++ Vol3B/WebScraping1/web_scraping1.py
@@ -0,0 +1,61 @@
+# web_scraping1.py
+"""Volume 3B: BeautifulSoup.
+<Name>
+<Class>
+<Date>
+"""
+
+# Problem 1
+def Prob1():
+    """Returns a list of tags used and the value of
+    the type attribute associated with the style tag
+    """
+    raise NotImplementedError("Problem 1 Incomplete")
+
+# Problem 2
+def Prob2():
+    """Prints (not returns) the prettified
+    string for the Three Stooges HTML.
+    """
+    raise NotImplementedError("Problem 2 Incomplete")
+
+# Problem 3
+def Prob3():
+    """Returns [u'title'] from the Three Stooges soup"""
+    raise NotImplementedError("Problem 3 Incomplete")
+
+# Problem 4
+def Prob4():
+    """Returns u'Mo' from the Three Stooges soup"""
+    raise NotImplementedError("Problem 4 Incomplete")
+
+# Problem 5
+def Prob5(method):
+    """Returns the u'More information...' using two different methods.
+    If method is 1, it uses first method. If method is 2, it uses
+    the second method.
+    """
+    raise NotImplementedError("Problem 5 Incomplete")
+
+# Problem 6
+def Prob6(method):
+    """Returns the tag associated with the "More information..."
+    link using two different methods. If method is 1, it uses the
+    first method. If method is 2, it uses the second method.
+    """
+    raise NotImplementedError("Problem 6 Incomplete")
+
+# Problem 7
+def Prob7():
+    """Loads 'SanDiegoWeather.htm' into BeautifulSoup and prints
+    (not returns) the tags referred to the in the Problem 7 questions.
+    """
+    raise NotImplementedError("Problem 7 Incomplete")
+
+# Problem 8
+def Prob8():
+    """Loads 'Big Data dates.htm' into BeautifulSoup and uses find_all()
+    and re to return a list of all tags containing links to bank data
+    from September 30, 2003 to December 31, 2014.
+    """
+    raise NotImplementedError("Problem 8 Incomplete")
