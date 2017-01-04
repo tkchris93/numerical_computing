4d2c2601e2835cb1bbe161476415aab3edb2700b
Shane <shanemcq18@gmail.com>
2017-01-04 15:40:11 -0700

    simplify markov driver

--- Vol2A/MarkovChains/testDriver.py
+++ Vol2A/MarkovChains/testDriver.py
@@ -1,49 +1,17 @@
 # testDriver.py
-"""Volume II: Markov Chains. Test driver."""
+"""Volume 2A: Markov Chains. Test Driver."""
 
-# Wrappers ====================================================================
-
-import signal
-from functools import wraps
-
-def _timeout(seconds):
-    """Decorator for preventing a function from running for too long.
-
-    Inputs:
-        seconds (int): The number of seconds allowed.
-
-    Notes:
-        This decorator uses signal.SIGALRM, which is only available on Unix.
-    """
-    assert isinstance(seconds, int), "@timeout(sec) requires an int"
-
-    class TimeoutError(Exception):
-        pass
-
-    def _handler(signum, frame):
-        """Handle the alarm by raising a custom exception."""
-        raise TimeoutError("Timeout after {0} seconds".format(seconds))
-
-    def decorator(func):
-        def wrapper(*args, **kwargs):
-            signal.signal(signal.SIGALRM, _handler)
-            signal.alarm(seconds)               # Set the alarm.
-            try:
-                result = func(*args, **kwargs)
-            finally:
-                signal.alarm(0)                 # Turn the alarm off.
-            return result
-        return wraps(func)(wrapper)
-    return decorator
-
-# Test Script and Class =======================================================
+import sys
+sys.path.insert(0, "../..")
+from base_test_driver import BaseTestDriver, _timeout
 
 import numpy as np
 from os import remove as rm
 from solutions import SentenceGenerator
 
-def test(student_module):
-    """Grade a student's entire solutions file.
+
+class TestDriver(BaseTestDriver):
+    """Class for testing a student's work.
 
      5 points for random_chain()
      5 points for forecast()
@@ -51,110 +19,32 @@ def test(student_module):
     10 points for steady_state()
     15 points for the SentenceGenerator class.
 
-    Inputs:
-        student_module: the imported module for the student's file.
-
-    Returns:
-        score (int): the student's score, out of 40.
-        feedback (str): a printout of test results for the student.
+    Grade the entire lab assignment at once via test_all(), or grade one
+    problem at a time via the different problemX() methods.
     """
-
-    tester = _testDriver()
-    tester.test_all(student_module)
-    return tester.score, tester.feedback
-
-class _testDriver(object):
-    """Class for testing a student's work.
-
-    Attributes:
-        Score (int)
-        Feedback (str)
-    """
-
     # Constructor -------------------------------------------------------------
     def __init__(self):
-        """Initialize the feedback attribute."""
-        self.feedback = ""
-
-    # Main routine ------------------------------------------------------------
-    def test_all(self, student_module, total=40):
-        """Grade the provided module on each problem and compile feedback."""
-        # Reset feedback and score.
-        self.feedback = ""
-        self.score = 0
-
-        def test_one(problem, label, value):
-            """Test a single problem, checking for errors."""
-            try:
-                self.feedback += "\n\n{} ({} points):".format(label, value)
-                points = problem(student_module)
-                self.score += points
-                self.feedback += "\nScore += {}".format(points)
-            except BaseException as e:
-                self.feedback += "\n{}: {}".format(self._errType(e), e)
-
-        # Grade each problem.
-        test_one(self.problem1, "Problem 1",                 5)
-        test_one(self.problem2, "Problem 2",                 5)
-        test_one(self.problem3, "Problem 3",                 5)
-        test_one(self.problem4, "Problem 4",                10)
-        test_one(self.problem6, "SentenceGenerator class",  15)
-
-        # Report final score.
-        percentage = (100. * self.score) / total
-        self.feedback += "\n\nTotal score: {}/{} = {}%".format(
-                                    self.score, total, round(percentage, 2))
-        if   percentage >=  98: self.feedback += "\n\nExcellent!"
-        elif percentage >=  90: self.feedback += "\n\nGreat job!"
-
-        # Add comments (optionally).
-        print(self.feedback)
-        comments = str(raw_input("Comments: "))
-        if len(comments) > 0:
-            self.feedback += '\n\n\nComments:\n\t{}'.format(comments)
+        """Initialize attributes."""
+        BaseTestDriver.__init__(self)
+        self.problems = [   (self.problem1, "Problem 1",  5),
+                            (self.problem2, "Problem 2",  5),
+                            (self.problem3, "Problem 3",  5),
+                            (self.problem4, "Problem 4", 10),
+                            (self.problem6, "SentenceGenerator", 15)    ]
 
     # Helper Functions --------------------------------------------------------
-    @staticmethod
-    def _errType(error):
-        """Get just the name of the exception 'error' in string format."""
-        return str(type(error).__name__)
+    def _addFeedback(self, correct, student, message):
+        """Add a message to the feedback, plus a description of the correct
+        answer versus the student's answer.
+        """
+        self.feedback += "\n{}".format(message)
+        self.feedback += "\n\tCorrect response:\n{}".format(correct)
+        self.feedback += "\n\tStudent response:\n{}".format(student)
 
     @staticmethod
     def random_chain(n):
         A = np.random.random((n,n))
         return A / A.sum(axis=0)
-
-    def _eqTest(self, correct, student, message):
-        """Test to see if 'correct' and 'student' are equal.
-        Report the given 'message' if they are not.
-        """
-        if np.allclose(correct, student):
-            return 1
-        else:
-            self.feedback += "\n{}".format(message)
-            self.feedback += "\n\tCorrect response: {}".format(correct)
-            self.feedback += "\n\tStudent response: {}".format(student)
-            return 0
-
-    def _grade(self, points, message=None):
-        """Manually grade a problem worth 'points'. Return the score.
-        If full points are not earned, get feedback on the problem.
-        """
-        credit = -1
-        while credit > points or credit < 0:
-            try:
-                credit = int(input("\nScore out of {}: ".format(points)))
-            except:
-                credit = -1
-        if credit != points:
-            # Add comments (optionally),
-            comments = raw_input("Comments: ")
-            if len(comments) > 0:
-                self.feedback += "\n{}".format(comments)
-            # Or add a predetermined error message.
-            elif message is not None:
-                self.feedback += "\n{}".format(message)
-        return credit
 
     # Problems ----------------------------------------------------------------
     @_timeout(2)
@@ -195,7 +85,7 @@ class _testDriver(object):
         """Test four_state_forecast(). 5 points."""
         def _test_one(n):
             """Check that four_state_forecast returns a list of length n."""
-            return self._eqTest(n, len(s.four_state_forecast(n)),
+            return self._eqTest(n-1, len(s.four_state_forecast(n)),
                      "four_state_forecast(n) should return a list of length n")
 
         return _test_one(5) + 2*_test_one(10) + 2*_test_one(100)
@@ -276,8 +166,31 @@ class _testDriver(object):
 
         return points
 
+# Main Routine ================================================================
+
+def test(student_module, total=40):
+    """Grade a student's entire solutions file.
+
+     5 points for random_chain()
+     5 points for forecast()
+     5 points for four_state_forecast()
+    10 points for steady_state()
+    15 points for the SentenceGenerator class.
+
+    Inputs:
+        student_module: the imported module for the student's file.
+        total (int): the total possible score.
+
+    Returns:
+        score (int): the student's score, out of 'total'.
+        feedback (str): a printout of results for the student.
+    """
+    tester = TestDriver()
+    tester.test_all(student_module, total)
+    return tester.score, tester.feedback
+
 # Validation ==================================================================
+
 if __name__ == '__main__':
-    """Validate the test driver by testing the solutions file."""
     import solutions
     test(solutions)
