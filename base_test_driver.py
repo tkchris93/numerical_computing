1895f0d4814ed4bdcc5300d31f3e5d36f9ee69a6
Shane <shanemcq18@gmail.com>
2017-01-04 17:51:51 -0700

    template driver

--- base_test_driver.py
+++ base_test_driver.py
@@ -23,8 +23,9 @@ each problem and collect feedback, but each problem can be graded individually
 via the different problemX() methods. This allows the instructor to grade from
 IPython, or to automate grading using Git.
 
-Customize the docstrings of the test() function and the test driver class to
-give specific instructions about how the lab is to be graded.
+See the TestDriver class and the test() function below as a template for new
+drivers. Customize the docstrings of the test() function and the test driver
+class to give specific instructions about how the lab is to be graded.
 
 Decorators --------------------------------------------------------------------
 
@@ -112,6 +113,21 @@ class BaseTestDriver(object):
                 accept as input the imported student module.
             label (str): a label for the problem or code being tested.
             value (int): the amount of points that the problem is worth.
+        _feedback_newlines (bool): If False, feedback appears as follows:
+
+            <message>
+                Correst Response: <correct>
+                Student Response: <student>
+
+            If True, feedback appears as follows:
+
+            <message>
+                Correst Response:
+            <correct>
+                Student Response:
+            <student>
+
+            Set to true for lengthy feedback, such as NumPy arrays.
 
     Main Methods:
         __init__(): Initialize all attributes. Must be overwritten by inherited
@@ -297,17 +313,43 @@ class BaseTestDriver(object):
         return credit
 
 
-# TODO: write a TestDriver class to copy as a template.
+# TEMPLATE
+"""For new test drivers, copy the following template class."""
 
+# import sys
+# sys.path.insert(0, "../..")
+# from base_test_driver import BaseTestDriver, [other imports]
+
+class TestDriver(BaseTestDriver):
+    """Class for testing a student's work.
+
+     5 points for problem 1
+    15 points for problem 2
+
+    Grade the entire lab assignment at once via test_all(), or grade one
+    problem at a time via the different problemX() methods.
+    """
+    # Constructor -------------------------------------------------------------
+    def __init__(self):
+        """Initialize attributes."""
+        BaseTestDriver.__init__(self)
+        self.problems = [   (self.problem1, "Problem 2",  5),
+                            (self.problem2, "Problem 2", 15)    ]
+
+    # Problems ----------------------------------------------------------------
+    def problem1(self, s):
+        pass
+
+    def problem2(self, s):
+        pass
 
 # Main Routine ================================================================
 
 def test(student_module, total=40):
     """Grade a student's entire solutions file.
 
-    X points for problem 1
-    X points for problem 2
-    ...
+     5 points for problem 1
+    15 points for problem 2
 
     Inputs:
         student_module: the imported module for the student's file.
@@ -315,19 +357,14 @@ def test(student_module, total=40):
 
     Returns:
         score (int): the student's score, out of 'total'.
-        feedback (str): a printout of test results for the student.
+        feedback (str): a printout of results for the student.
     """
-    tester = BaseTestDriver()
+    tester = TestDriver()
     tester.test_all(student_module, total)
     return tester.score, tester.feedback
 
 # Validation ==================================================================
 
 if __name__ == '__main__':
-    """Validate the test driver by testing the solutions file."""
     import solutions
-    # To use IPython for validation, include these lines:
-    # from imp import reload        # Python 3.0-3.3
-    # from importlib import reload  # Python 3.4+
-    # reload(solutions)
     test(solutions)
