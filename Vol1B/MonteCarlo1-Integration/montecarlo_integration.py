bc82edc8dee5e79331d6e21bae14b2f4c0e70042
Shane <shanemcq18@gmail.com>
2017-01-06 01:11:23 -0700

    update mc1 spec

--- Vol1B/MonteCarlo1-Integration/montecarlo_integration.py
+++ Vol1B/MonteCarlo1-Integration/montecarlo_integration.py
@@ -1,5 +1,5 @@
 # montecarlo_integration.py
-"""Volume 1B: Monte Carlo 1 (Integration).
+"""Volume 1B: Monte Carlo Integration.
 <Name>
 <Class>
 <Date>
@@ -7,36 +7,44 @@
 
 
 # Problem 1
-def prob1():
+def prob1(N=10000):
     """Return an estimate of the volume of the unit sphere using Monte
     Carlo Integration.
 
-    Inputs:
-        numPoints (int, optional) - Number of points to sample. Defaults
-            to 10^5.
-    Returns:
-        volume (int) - Approximate value of the area of the unit sphere.
+    Input:
+        N (int, optional) - The number of points to sample. Defaults
+            to 10000.
+
     """
     raise NotImplementedError("Problem 1 Incomplete")
 
 
 # Problem 2
-def prob2():
-    """Return an estimate of the area under the curve,
-    f(x) = |sin(10x)cos(10x) + sqrt(x)*sin(3x)| from 1 to 5.
+def prob2(f, a, b, N=10000):
+    """Use Monte-Carlo integration to approximate the integral of
+    1-D function f on the interval [a,b].
 
     Inputs:
-        numPoints (int, optional) - Number of points to sample. Defautls
-            to 10^5.
+        f (function) - Function to integrate. Should take scalar input.
+        a (float) - Left-hand side of interval.
+        b (float) - Right-hand side of interval.
+        N (int, optional) - The number of points to sample in
+            the Monte-Carlo method. Defaults to 10000.
+
     Returns:
-        area (int) - Apprimate value of the area under the
-            specified curve.
+        estimate (float) - The result of the Monte-Carlo algorithm.
+
+    Example:
+        >>> f = lambda x: x**2
+        >>> # Integral from 0 to 1. True value is 1/3.
+        >>> prob2(f, 0, 1)
+        0.3333057231764805
     """
     raise NotImplementedError("Problem 2 Incomplete")
 
 
 # Problem 3
-def mc_int(f, mins, maxs, numPoints=500, numIters=100):
+def prob3(f, mins, maxs, N=10000):
     """Use Monte-Carlo integration to approximate the integral of f
     on the box defined by mins and maxs.
 
@@ -45,37 +53,32 @@ def mc_int(f, mins, maxs, numPoints=500, numIters=100):
             accept a 1-D NumPy array as input.
         mins (1-D np.ndarray) - Minimum bounds on integration.
         maxs (1-D np.ndarray) - Maximum bounds on integration.
-        numPoints (int, optional) - The number of points to sample in
-            the Monte-Carlo method. Defaults to 500.
-        numIters (int, optional) - An integer specifying the number of
-            times to run the Monte Carlo algorithm. Defaults to 100.
+        N (int, optional) - The number of points to sample in
+            the Monte-Carlo method. Defaults to 10000.
 
     Returns:
-        estimate (int) - The average of 'numIters' runs of the
-            Monte-Carlo algorithm.
+        estimate (float) - The result of the Monte-Carlo algorithm.
 
     Example:
         >>> f = lambda x: np.hypot(x[0], x[1]) <= 1
-        >>> # Integral over the square [-1,1] x [-1,1]. Should be pi.
+        >>> # Integral over the square [-1,1] x [-1,1]. True value is pi.
         >>> mc_int(f, np.array([-1,-1]), np.array([1,1]))
         3.1290400000000007
     """
-    raise NotImplementedError("Problem 3 Incomplete")
+    NotImplementedError("Problem 3 Incomplete")
 
 
 # Problem 4
-def prob4(numPoints=[500]):
-    """Calculates an estimate of the integral of
-    f(x,y,z,w) = sin(x)y^5 - y^5 + zw + yz^3
+def prob4():
+    """Integrate the joint normal distribution.
 
-    Inputs:
-        numPoints (list, optional) - a list of the number of points to
-            use the approximation. Defaults to [500].
-    Returns:
-        errors (list) - a list of the errors when calculating the
-            approximation using 'numPoints' points.
-    Example:
-    >>> prob4([100,200,300])
-    [-0.061492011289160729, 0.016174426377108819, -0.0014292910207835802]
+    Return your Monte Carlo estimate, SciPy’s answer, and (assuming SciPy is
+    correct) the relative error of your Monte Carlo estimate.
     """
-    raise NotImplementedError("Problem 4 Incomplete")
+    NotImplementedError("Problem 4 Incomplete")
+
+
+# Problem 5
+def prob5(numEstimates=50):
+    """Plot the error of Monte Carlo Integration."""
+    NotImplementedError("Problem 5 Incomplete")
