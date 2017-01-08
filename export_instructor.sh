9a8c07891f91289c67a3c427228ba87ea1dd44b0
Shane <shanemcq18@gmail.com>
2017-01-07 22:50:19 -0700

    double travis, update instructor exports

--- export_instructor.sh
+++ export_instructor.sh
@@ -7,11 +7,17 @@ else
     # Solutions files (.py and .ipynb)
     find . -type f -name "solutions.py" | cpio -pdv $1
     find . -type f -name "solutions.ipynb" | cpio -pdv $1
-    # Test drivers TODO: "*test_driver*.py"
-    find . -type f -name "testDriver.py" | cpio -pdv $1
+
+    # Test drivers
+    find . -type f -name "*test_driver*.py" | cpio -pdv $1
+
     # Instructor Notes
     find . -type f -name "*[Nn]otes.tex" | cpio -pdv $1
     cp SIAM-GH-book.cls $1
     cp command.tex $1
     cp *.pdf $1
+
+    # Remove extra directories
+    rm -rf $1/Orphans
+    rm -rf $1/Appendices
 fi
